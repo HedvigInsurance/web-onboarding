cb869d6edd887dd6d6bf26d02a9a88371151b51c
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-09-24 11:07:13 +0200

    Bump hedvig cli and upgrade config

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,9 @@ module.exports = {
     '@babel/preset-react',
     ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
   ],
+  plugins: [
+    'react-hot-loader/babel'
+  ],
   env: {
     test: {
       plugins: ['@babel/transform-modules-commonjs'],
