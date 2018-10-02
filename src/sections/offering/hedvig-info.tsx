0d6fc4e2d2180028febd931732aa3b466b0b0659
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-10-02 14:33:02 +0200

    -

--- src/sections/offering/hedvig-info.tsx
+++ src/sections/offering/hedvig-info.tsx
@@ -28,12 +28,12 @@ export const Col = styled('div')({
 
 const Image = styled('img')({
   margin: '20px',
-  maxWidth: '200px',
+  maxWidth: '210px',
   '@media (max-width: 710px)': {
     maxWidth: '300px',
   },
   '@media (max-width: 400px)': {
-    maxWidth: '200px',
+    maxWidth: '210px',
   },
 })
 
@@ -42,14 +42,14 @@ export const Title = styled('label')({
   fontSize: '16px',
   textAlign: 'center',
   color: colors.BLACK,
-  maxWidth: '200px',
+  maxWidth: '210px',
 })
 
 export const SubTitle = styled('label')({
   fontSize: '16px',
   textAlign: 'center',
   color: colors.DARK_GRAY,
-  maxWidth: '200px',
+  maxWidth: '210px',
 })
 
 export const HedvigInfo: React.SFC<Props> = (props) => (
@@ -73,21 +73,21 @@ export const HedvigInfo: React.SFC<Props> = (props) => (
 
 const COLUMNS = [
   {
-    title: 'Anmäl skador snabbt i vår chatt',
+    title: 'Anmäl skador och få betalt på nolltid',
     subTitle:
-      'Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
+      'Med vår smarta chattbot baserad på AI och maskininlärning kan vi snabbt göra bedömningar av skador och betala ut pengarna till dig utan krångliga formulär eller väntetider.',
     image: '/assets/offering/Placeholder.png',
   },
   {
-    title: 'Schyssta villkor som människor förstår',
+    title: 'Schysstare affärsmodell',
     subTitle:
-      'Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar.',
+      'Hedvig gör saker annorlunda än andra försäkringsbolag, istället för att överskottet från alla premier går till vår vinst tar vi en månatlig fast avgift. Därför har vi inget incitament till att hålla inne med pengar!',
     image: '/assets/offering/Placeholder.png',
   },
   {
     title: 'Överskottet går till välgörenhet',
     subTitle:
-      'Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark och senare med mjukvaror som Aldus PageMaker.',
+      'Det som blir över efter årets skadeutbetalningar går till en välgörenhetsorganisation som du själv  väljer.',
     image: '/assets/offering/Placeholder.png',
   },
 ]
