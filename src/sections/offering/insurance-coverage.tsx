c3398ac27771b1f4083f4bfed623399c7f15bcef
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-10-04 13:54:44 +0200

    First iteration peril dropdown

--- src/sections/offering/insurance-coverage.tsx
+++ src/sections/offering/insurance-coverage.tsx
@@ -2,6 +2,7 @@ import { colors, fonts } from '@hedviginsurance/brand'
 import { Card } from 'components/offering/card'
 import { Header } from 'components/offering/header'
 import { SubTitle } from 'components/offering/sub-title'
+import { ActionMap, Container } from 'constate'
 import * as React from 'react'
 import styled from 'react-emotion'
 
@@ -10,7 +11,7 @@ interface Props {
   subTitle: string
 }
 
-const Container = styled('div')({
+const OuterContainer = styled('div')({
   backgroundColor: colors.OFF_WHITE,
 })
 
@@ -78,6 +79,17 @@ const Label = styled('p')({
   color: colors.DARK_GRAY,
 })
 
+const DropDownText = styled('div')({
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginBottom: '60px',
+  marginTop: '0px',
+  fontSize: '16px',
+  textAlign: 'center',
+  maxWidth: '700px',
+  color: colors.DARK_GRAY,
+})
+
 const CARDS = [
   {
     key: 0,
@@ -88,26 +100,36 @@ const CARDS = [
         key: 0,
         title: 'Juridisk tvist',
         image: '/assets/offering/juridisk_tvist.png',
+        expandableText:
+          '0,0. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 1,
         title: 'Överfall',
         image: '/assets/offering/overfall_2.png',
+        expandableText:
+          '0,1. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 2,
         title: 'Sjuk på resa',
         image: '/assets/offering/sjuk_pa_resa.png',
+        expandableText:
+          '0,2. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 3,
         title: 'Försenad resa',
         image: '/assets/offering/forsenad_resa.png',
+        expandableText:
+          '0,3. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 4,
         title: 'Försenat bagage',
         image: '/assets/offering/forsenad_resa.png',
+        expandableText:
+          '0,4. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
     ],
   },
@@ -120,21 +142,29 @@ const CARDS = [
         key: 0,
         title: 'Eldsvåda',
         image: '/assets/offering/eldsvada.png',
+        expandableText:
+          '1,0. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 1,
         title: 'Inbrott',
         image: '/assets/offering/inbrott.png',
+        expandableText:
+          '1,1. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 2,
         title: 'Oväder',
         image: '/assets/offering/ovader.png',
+        expandableText:
+          '1,2. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 3,
         title: 'Skadedjur',
         image: '/assets/offering/skadedjur.png',
+        expandableText:
+          '1,3. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
     ],
   },
@@ -147,56 +177,133 @@ const CARDS = [
         key: 0,
         title: 'Drulle',
         image: '/assets/offering/Drulle.png',
+        expandableText:
+          '2,0. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 1,
         title: 'Eldsvåda',
         image: '/assets/offering/eldsvada_green.png',
+        expandableText:
+          '2,1. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 2,
         title: 'Oväder',
         image: '/assets/offering/ovader_green.png',
+        expandableText:
+          '2,2. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 3,
         title: 'Stöld',
         image: '/assets/offering/stold.png',
+        expandableText:
+          '2,3. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
       {
         key: 4,
         title: 'Vattenläcka',
         image: '/assets/offering/vattenlacka.png',
+        expandableText:
+          '2,4. Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet.',
       },
     ],
   },
 ]
 
+interface State {
+  showIconNumber: number
+  showPerilNumber: number
+  textToShow: string
+}
+interface Actions {
+  handleIconClick: (peril: number, icon: number, text: string) => void
+}
+
 export const InsuranceCoverage: React.SFC<Props> = (props) => (
-  <Container>
-    <InnerContainer>
-      <Card>
-        <Header>{props.headline}</Header>
-        <SubTitle>{props.subTitle}</SubTitle>
-        {CARDS.map((card) => (
-          <BigRow key={card.key}>
-            <TitleRow>
-              <Col>
-                <ImageIcon src={card.icon} />
-                <CardHeader>{card.name}</CardHeader>
-              </Col>
-            </TitleRow>
-            <Row>
-              {card.icons.map((column) => (
-                <Col key={column.key}>
-                  <ImageIcon src={column.image} />
-                  <Label>{column.title}</Label>
-                </Col>
+  <OuterContainer>
+    <Container<State, ActionMap<State, Actions>>
+      initialState={{ showPerilNumber: undefined }}
+      actions={{
+        handleIconClick: (peril: number, icon: number, text: string) => () => ({
+          showPerilNumber: peril,
+          showIconNumber: icon,
+          textToShow: text,
+        }),
+      }}
+    >
+      {(state) => (
+        <OuterContainer>
+          <InnerContainer>
+            <Card>
+              <Header>{props.headline}</Header>
+              <SubTitle>{props.subTitle}</SubTitle>
+              {CARDS.map((card) => (
+                <BigRow key={card.key}>
+                  <TitleRow>
+                    <Col>
+                      <ImageIcon src={card.icon} />
+                      <CardHeader>{card.name}</CardHeader>
+                    </Col>
+                  </TitleRow>
+                  <Row>
+                    {card.icons.map((column) => (
+                      <Col
+                        key={column.key}
+                        onClick={() =>
+                          state.handleIconClick(
+                            card.key,
+                            column.key,
+                            column.expandableText,
+                          )
+                        }
+                      >
+                        <ImageIcon src={column.image} />
+                        <Label>{column.title}</Label>
+                      </Col>
+                    ))}
+                  </Row>
+                  {state.showPerilNumber === card.key ? (
+                    <DropDownText>{state.textToShow}</DropDownText>
+                  ) : null}
+                </BigRow>
               ))}
-            </Row>
-          </BigRow>
-        ))}
-      </Card>
-    </InnerContainer>
-  </Container>
+            </Card>
+          </InnerContainer>
+        </OuterContainer>
+      )}
+    </Container>
+  </OuterContainer>
 )
+
+export const InsuranceCoverageOld: React.SFC<Props> = (props) => {
+  return (
+    <OuterContainer>
+      <InnerContainer>
+        <Card>
+          <Header>{props.headline}</Header>
+          <SubTitle>{props.subTitle}</SubTitle>
+          {CARDS.map((card) => (
+            <BigRow key={card.key}>
+              <TitleRow>
+                <Col>
+                  <ImageIcon src={card.icon} />
+                  <CardHeader>{card.name}</CardHeader>
+                </Col>
+              </TitleRow>
+              <Row>
+                {card.icons.map((column) => (
+                  <Col key={column.key}>
+                    <ImageIcon src={column.image} />
+                    <Label>{column.title}</Label>
+                  </Col>
+                ))}
+              </Row>
+            </BigRow>
+          ))}
+        </Card>
+      </InnerContainer>
+    </OuterContainer>
+  )
+}
