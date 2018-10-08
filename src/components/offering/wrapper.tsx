fdd3a95b5e9595f4db3726eff38a96827e3d8363
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-10-08 12:48:05 +0200

    name change fix

--- src/components/offering/wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { colors } from '@hedviginsurance/brand'
-import styled from 'react-emotion'
-
-export const Wrapper = styled('div')({
-  width: '100%',
-  backgroundColor: colors.OFF_WHITE,
-})
