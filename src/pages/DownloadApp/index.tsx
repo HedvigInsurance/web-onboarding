efe9c5299451e396af699f0e04fc1548afcab284
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-10-09 16:51:03 +0200

    initial commit

---
+++ src/pages/DownloadApp/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import styled from 'react-emotion'
+
+const Wrapper = styled('div')({})
+
+export const DownloadApp: React.SFC<{}> = () => <Wrapper>hej</Wrapper>
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { Chat } from './pages/Chat'
+import { DownloadApp } from './pages/DownloadApp'
 import { Offering } from './pages/Offer'
 
 export const reactPageRoutes = [
   { path: '/hedvig', Component: Chat, exact: true },
   { path: '/offer', Component: Offering, exact: true },
+  { path: '/downloadapp', Component: DownloadApp, exact: true },
 ]
