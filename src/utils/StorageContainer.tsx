d6025d61ed6835a02b3ed0d1916875e6a2457180
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-09 17:16:54 +0200

    Make mockstorage interactive and refactor some storage stuff

--- src/utils/StorageContainer.tsx
+++ src/utils/StorageContainer.tsx
@@ -1,7 +1,38 @@
-import { Container, ContainerProps } from 'constate'
+import { Container, ContainerProps, EffectMap, EffectProps } from 'constate'
 import * as React from 'react'
-import { StorageState } from '../App'
+import { IsomorphicSessionStorage, Session } from './sessionStorage'
 
-export const StorageContainer: React.SFC<ContainerProps<StorageState>> = (
-  props,
-) => <Container<StorageState> context="storage" {...props} />
+export interface StorageState {
+  session: IsomorphicSessionStorage<Session>
+}
+
+export interface WithStorageProps {
+  storage: StorageState
+}
+
+interface StorageEffects {
+  setToken: (token: string) => void
+}
+
+export const StorageContainer: React.SFC<
+  ContainerProps<StorageState, {}, {}, EffectMap<StorageState, StorageEffects>>
+> = (props) => (
+  <Container<StorageState, {}, {}, EffectMap<StorageState, StorageEffects>>
+    context="storage"
+    {...props}
+    effects={
+      {
+        setToken: (token: string) => ({
+          state,
+          setState,
+        }: EffectProps<StorageState>) => {
+          state.session.setSession({
+            ...((state.session.getSession() || {}) as any),
+            token,
+          })
+          setState({ session: state.session })
+        },
+      } as any
+    }
+  />
+)
