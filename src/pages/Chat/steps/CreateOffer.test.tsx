655a1a9d34885e9276eb5648151efb333586bdc9
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-12 09:03:41 +0200

    Add session stuff to createOffercontainer

--- src/pages/Chat/steps/CreateOffer.test.tsx
+++ src/pages/Chat/steps/CreateOffer.test.tsx
@@ -1,15 +1,22 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { split } from 'apollo-link'
+import { Provider } from 'constate'
 import { mount } from 'enzyme'
 import * as React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { MockLink, MockSubscriptionLink } from 'utils/react-apollo-test-links'
 import { mockNetworkWait } from 'utils/test-utils'
+import { createSession, SESSION_KEY } from '../../../utils/sessionStorage'
+import { MockStorage } from '../../../utils/storage/MockStorage'
+import { StorageState } from '../../../utils/StorageContainer'
 import { createCreateOfferMutationMock } from '../utils/test-utils'
 import { CreateOffer } from './CreateOffer'
 
-it('renders without 💥', async () => {
+jest.mock('client/apolloClient', () => ({
+  apolloClient: { subscriptionClient: {} },
+}))
+it('creates an offer without 💥', async () => {
   const mocks = createCreateOfferMutationMock()
   const subscriptionLink = new MockSubscriptionLink()
   const mockLink = new MockLink(mocks)
@@ -25,7 +32,19 @@ it('renders without 💥', async () => {
 
   const wrapper = mount(
     <ApolloProvider client={client}>
-      <CreateOffer />
+      <Provider<{ storage: StorageState }>
+        initialState={{
+          storage: {
+            session: createSession(
+              new MockStorage({
+                [SESSION_KEY]: JSON.stringify({ token: 'abc123' }),
+              }),
+            ),
+          },
+        }}
+      >
+        <CreateOffer />
+      </Provider>
     </ApolloProvider>,
   )
 
