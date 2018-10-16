95cad604eda30785d0bdb773de2d3b54b1a33ead
Oscar Nylander <oscar@hedvig.com>
2018-10-16 23:00:08 +0200

    More wip on the tests

--- src/pages/Offer/index.test.tsx
+++ src/pages/Offer/index.test.tsx
@@ -2,13 +2,13 @@ import { Provider } from 'constate'
 import { mount } from 'enzyme'
 import * as React from 'react'
 import { MockedProvider, MockedResponse } from 'react-apollo/test-utils'
+import { HelmetProvider } from 'react-helmet-async'
 import { Redirect, StaticRouter } from 'react-router-dom'
 import { createSession, SESSION_KEY } from 'utils/sessionStorage'
 import { MockStorage } from 'utils/storage/MockStorage'
 import { mockNetworkWait } from 'utils/test-utils'
-import { Offering } from '.'
+import { OFFER_QUERY, Offering } from '.'
 import { Offer } from './sections/Offer'
-
 jest.mock('client/apolloClient', () => ({
   apolloClient: {
     subscriptionClient: { close: jest.fn() },
@@ -37,33 +37,52 @@ it('Queries when it has a session', async () => {
   const mocks: MockedResponse[] = [
     {
       request: {
-        query,
+        query: OFFER_QUERY,
+      },
+      result: {
+        data: {
+          insurance: {
+            address: 'Testvägen 1',
+            monthlyCost: 99,
+            insuredAtOtherCompany: true,
+            type: 'RENT',
+            postalNumber: '12345',
+            __typename: 'Insurance',
+          },
+          member: {
+            firstName: 'Test',
+            lastName: 'Testerson',
+            __typename: 'Member',
+          },
+        },
       },
     },
   ]
   const wrapper = mount(
-    <StaticRouter context={{}}>
-      <MockedProvider>
-        <Provider
-          initialState={{
-            storage: {
-              session: createSession(
-                new MockStorage({
-                  [SESSION_KEY]: JSON.stringify({ token: 'test-token' }),
-                }),
-              ),
-            },
-          }}
-        >
-          <Offering />
-        </Provider>
-      </MockedProvider>
-    </StaticRouter>,
+    <HelmetProvider>
+      <StaticRouter context={{}}>
+        <MockedProvider mocks={mocks}>
+          <Provider
+            initialState={{
+              storage: {
+                session: createSession(
+                  new MockStorage({
+                    [SESSION_KEY]: JSON.stringify({ token: 'test-token' }),
+                  }),
+                ),
+              },
+            }}
+          >
+            <Offering />
+          </Provider>
+        </MockedProvider>
+      </StaticRouter>
+    </HelmetProvider>,
   )
 
   await mockNetworkWait()
   wrapper.update()
-  console.log(wrapper.debug())
   expect(wrapper.find(Redirect)).toHaveLength(0)
-  expect(wrapper.find(Offer)).toHaveLength(0)
+  console.log(wrapper.debug())
+  expect(wrapper.find(Offer)).toHaveLength(1)
 })
