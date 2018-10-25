f6c5fafbe37f1b2ba01341c29bf0b2ae1bc84b1f
Fredrik Holmdahl <holmdahl.fredrik@gmail.com>
2018-10-25 14:51:25 +0200

    Items on top bar floats consistent with resize

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -17,7 +17,7 @@ const Bar = styled('div')({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'space-between',
+  justifyContent: 'end',
   top: 0,
   left: 0,
   right: 0,
--- src/pages/Chat/ResetButton.tsx
+++ src/pages/Chat/ResetButton.tsx
@@ -5,6 +5,13 @@ import { ChatContainer } from './state'
 const Wrapper = styled('div')({
   marginRight: 26,
   textAlign: 'right',
+  width: '20%',
+  '@media (max-width: 850px)': {
+    width: '33%',
+  },
+  '@media (max-width: 600px)': {
+    width: '50%',
+  },
 })
 
 const Button = styled('button')({
