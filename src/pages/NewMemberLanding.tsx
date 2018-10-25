f5bdf1102dd7ba0ceb7d080262ef1fb92747d3d9
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-25 16:22:03 +0200

    Redirect /new-member -> hedvig

---
+++ src/pages/NewMemberLanding.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+import { Redirect } from 'react-router-dom'
+
+export const NewMemberLanding: React.SFC = () => <Redirect to="/new-member/hedvig" />
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Chat } from './pages/Chat'
 import { Download } from './pages/Download'
 import { FourOhFour } from './pages/FourOhFour'
+import { NewMemberLanding } from './pages/NewMemberLanding'
 import { Offering } from './pages/Offer'
 import { Sign } from './pages/Sign'
 
 export const reactPageRoutes = [
+  { path: '/new-member', Component: NewMemberLanding, exact: true },
   { path: '/new-member/hedvig', Component: Chat, exact: true },
   { path: '/new-member/offer', Component: Offering, exact: true },
   { path: '/new-member/download', Component: Download, exact: true },
