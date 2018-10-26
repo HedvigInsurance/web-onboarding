0ae536193c46f80ac1a93298d1a2ecda0da5b5c0
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-26 16:42:21 +0200

    Add page titles to download and sign

--- src/pages/Sign/index.tsx
+++ src/pages/Sign/index.tsx
@@ -1,10 +1,19 @@
+import { TranslationsConsumer } from '@hedviginsurance/textkeyfy'
 import { TopBar } from 'components/TopBar'
 import { Page } from 'components/utils/Page'
 import * as React from 'react'
+import Helmet from 'react-helmet-async'
 import { SignUp } from './sections/SignUp'
 
 export const Sign: React.SFC = () => (
   <Page>
+    <TranslationsConsumer textKey="SIGN_PAGE_TITLE">
+      {(t) => (
+        <Helmet>
+          <title>{t}</title>
+        </Helmet>
+      )}
+    </TranslationsConsumer>
     <TopBar progress={2} />
     <SignUp />
   </Page>
