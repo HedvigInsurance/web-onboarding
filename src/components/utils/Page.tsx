000a647b232c225bfc740aeab74d68fc19e9311c
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-26 15:36:47 +0200

    Reset scroll position when mounting page

---
+++ src/components/utils/Page.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Mount } from 'react-lifecycle-components'
+
+export const Page: React.SFC = ({ children }) => {
+  return (
+    <Mount
+      on={() => {
+        // JSDOM weirdness writes to stderr if one tries to scroll in test
+        if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'test') {
+          window.scrollTo(0, 0)
+        }
+      }}
+    >
+      {children}
+    </Mount>
+  )
+}
