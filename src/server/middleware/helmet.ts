a90716f3d291f8290304ae89957e8df2537b4851
Johan Palmfjord <johan.palmfjord@hedvig.com>
2018-10-29 14:50:30 +0100

    Add more csp allowed targets

--- src/server/middleware/helmet.ts
+++ src/server/middleware/helmet.ts
@@ -1,26 +1,34 @@
 import * as koaHelmet from 'koa-helmet'
 import * as uuidV4 from 'uuid/v4'
 
+const defaultSrc = [
+  "'self'",
+  'cdn.hedvig.com',
+  process.env.GIRAFFE_ENDPOINT as string,
+  process.env.GIRAFFE_WS_ENDPOINT as string,
+]
+
 export const helmet = koaHelmet({
   contentSecurityPolicy: {
     directives: {
-      defaultSrc: [
-        "'self'",
-        'cdn.hedvig.com',
-        process.env.GIRAFFE_ENDPOINT as string,
-        process.env.GIRAFFE_WS_ENDPOINT as string,
-      ],
+      defaultSrc,
       scriptSrc: [
         "'self'",
         "'unsafe-eval'",
         'cdn.hedvig.com',
         'browser.sentry-cdn.com',
         'cdn.segment.com',
+        'www.googletagmanager.com',
         // tslint:disable-next-line variable-name
         (_request, response) => {
           ;(response as any).cspNonce = uuidV4()
           return `'nonce-${(response as any).cspNonce}'`
         },
+      ],
+      connectSrc: [
+        ...defaultSrc,
+        'https://api.segment.io',
+        'https://sentry.io/api',
       ],
       styleSrc: ["'self'", "'unsafe-inline'", 'cdn.hedvig.com'],
       upgradeInsecureRequests: true,
