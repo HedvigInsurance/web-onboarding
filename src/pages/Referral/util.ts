373437fd1c6f7f5a2d540208448a69175dedddda
Oscar Nylander <oscar@hedvig.com>
2019-06-18 09:56:53 +0200

    Make it better

--- src/pages/Referral/util.ts
+++ src/pages/Referral/util.ts
@@ -4,16 +4,20 @@ export const getFirebaseLinkDomain = () => {
     (window as any).FIREBASE_LINK_DOMAIN !== undefined
   ) {
     return (window as any).FIREBASE_LINK_DOMAIN
-  } else if (process.env.NODE_ENV === 'development') {
+  }
+
+  if (process.env.NODE_ENV === 'development') {
     return process.env.FIREBASE_LINK_DOMAIN || 'https://hedvigtest.page.link'
-  } else if (
+  }
+
+  if (
     process.env.NODE_ENV !== 'development' &&
     process.env.FIREBASE_LINK_DOMAIN
   ) {
     return process.env.FIREBASE_LINK_DOMAIN
-  } else {
-    throw Error(
-      'Unable to find firebase link domain - specify with envvar `FIREBASE_LINK_DOMAIN`',
-    )
   }
+
+  throw Error(
+    'Unable to find firebase link domain - specify with envvar `FIREBASE_LINK_DOMAIN`',
+  )
 }
