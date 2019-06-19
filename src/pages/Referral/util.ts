6ca1d73890c26e138cea19c7a3428e1250a127eb
Oscar Nylander <oscar@hedvig.com>
2019-06-19 16:34:36 +0200

    Fix firebase links
    I had made bad assumptions about how they work.

--- src/pages/Referral/util.ts
+++ src/pages/Referral/util.ts
@@ -1,3 +1,13 @@
+export const createFirebaseLink = (code: string) => {
+  const encodedLink = encodeURIComponent(
+    `https://www.hedvig.com/referrals?code=${code}`,
+  )
+  const encodedApn = encodeURIComponent(getAndroidPackageName())
+  const encodedIbi = encodeURIComponent(getAppleBundleId())
+  const encodedIsi = encodeURIComponent(getAppStoreId())
+
+  return `${getFirebaseLinkDomain()}/?link=${encodedLink}&apn=${encodedApn}&ibi=${encodedIbi}&isi=${encodedIsi}`
+}
 export const getFirebaseLinkDomain = () => {
   if (
     typeof window !== 'undefined' &&
@@ -19,5 +29,71 @@ export const getFirebaseLinkDomain = () => {
 
   throw Error(
     'Unable to find firebase link domain - specify with envvar `FIREBASE_LINK_DOMAIN`',
+  )
+}
+
+export const getAndroidPackageName = () => {
+  if (
+    typeof window !== 'undefined' &&
+    (window as any).ANDROID_PACKAGE_NAME !== undefined
+  ) {
+    return (window as any).ANDROID_PACKAGE_NAME
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    return process.env.ANDROID_PACKAGE_NAME || 'com.hedvig.dev.app'
+  }
+
+  if (
+    process.env.NODE_ENV !== 'development' &&
+    process.env.ANDROID_PACKAGE_NAME
+  ) {
+    return process.env.ANDROID_PACKAGE_NAME
+  }
+
+  throw Error(
+    'Unable to find android package name - specify with envvar `ANDROID_PACKAGE_NAME`',
+  )
+}
+
+export const getAppleBundleId = () => {
+  if (
+    typeof window !== 'undefined' &&
+    (window as any).APPLE_BUNDLE_ID !== undefined
+  ) {
+    return (window as any).APPLE_BUNDLE_ID
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    return process.env.APPLE_BUNDLE_ID || 'com.hedvig.test.app'
+  }
+
+  if (process.env.NODE_ENV !== 'development' && process.env.APPLE_BUNDLE_ID) {
+    return process.env.APPLE_BUNDLE_ID
+  }
+
+  throw Error(
+    'Unable to find apple bundle id - specify with envvar `APPLE_BUNDLE_ID`',
+  )
+}
+
+export const getAppStoreId = () => {
+  if (
+    typeof window !== 'undefined' &&
+    (window as any).APP_STORE_ID !== undefined
+  ) {
+    return (window as any).APP_STORE_ID
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    return process.env.APP_STORE_ID || '1339670517'
+  }
+
+  if (process.env.NODE_ENV !== 'development' && process.env.APP_STORE_ID) {
+    return process.env.APP_STORE_ID
+  }
+
+  throw Error(
+    'Unable to find apple bundle id - specify with envvar `APPLE_BUNDLE_ID`',
   )
 }
