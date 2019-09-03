7ca1db0e43868b7c2883b81aab3e956fad57628e
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-09-03 11:30:55 +0200

    fixed linting issue

--- src/client/apolloClient.ts
+++ src/client/apolloClient.ts
@@ -1,12 +1,17 @@
-import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher,
+} from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { WebSocketLink } from 'apollo-link-ws'
 import { CookieStorage } from 'cookie-storage'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { createSession, Session } from '../utils/sessionStorage'
-import introspectionData from './fragmentTypes';
+import introspectionData from './fragmentTypes'
 
-const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData: introspectionData })
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData: introspectionData,
+})
 
 export const apolloClient = (() => {
   if (typeof window === 'undefined') {
@@ -27,7 +32,9 @@ export const apolloClient = (() => {
     },
   )
   const client = new ApolloClient({
-    cache: new InMemoryCache({ fragmentMatcher }).restore((window as any).__INITIAL_STATE__),
+    cache: new InMemoryCache({ fragmentMatcher }).restore(
+      (window as any).__INITIAL_STATE__,
+    ),
     link: new WebSocketLink(subscriptionClient),
   })
 
--- src/client/fragmentTypes.ts
+++ src/client/fragmentTypes.ts
@@ -1,18 +1,18 @@
 export default {
-    __schema: {
-        types: [
-            {
-                kind: "UNION",
-                name: "Incentive",
-                possibleTypes: [
-                    {
-                        name: "FreeMonths"
-                    },
-                    {
-                        name: "MonthlyCostDeduction"
-                    }
-                ]
-            }
-        ]
-    }
-} 
+  schema: {
+    types: [
+      {
+        kind: 'UNION',
+        name: 'Incentive',
+        possibleTypes: [
+          {
+            name: 'FreeMonths',
+          },
+          {
+            name: 'MonthlyCostDeduction',
+          },
+        ],
+      },
+    ],
+  },
+}
