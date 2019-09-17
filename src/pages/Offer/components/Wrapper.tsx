e8af3d02efadd73fa3c8948c2a23f84ccb02648e
Gustaf Gunér <gustaf@guner.se>
2019-09-17 15:57:21 +0200

    Make discount bubble responsive and adjust card spacing

--- src/pages/Offer/components/Wrapper.tsx
+++ src/pages/Offer/components/Wrapper.tsx
@@ -4,4 +4,10 @@ import styled from 'react-emotion'
 export const Wrapper = styled('div')({
   width: '100%',
   backgroundColor: colors.OFF_WHITE,
+  paddingLeft: '20px',
+  paddingRight: '20px',
+  '@media (max-width: 350px)': {
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
 })
