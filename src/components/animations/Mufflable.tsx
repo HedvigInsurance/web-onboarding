3c054fbe83b6e0afdef546f3edfef18747abae75
Gustaf Gunér <gustaf@guner.se>
2019-11-19 16:17:00 +0100

    Change back style

--- src/components/animations/Mufflable.tsx
+++ src/components/animations/Mufflable.tsx
@@ -7,11 +7,19 @@ interface MufflableProps {
   direction?: string
 }
 
-export const Mufflable = styled.div<MufflableProps>`
-  opacity: ${(props) => (props.muffled && !props.unMuffled ? 0.5 : 1)};
-  font-size: ${(props) => (props.muffled ? 16 : 12)};
-  transition: opacity 200ms, font 200ms;
-  :hover {
-    opacity: ${(props) => (props.unMufflable ? 1 : undefined)};
-  }
-`
+export const Mufflable = styled('div')<MufflableProps>(
+  ({
+    muffled,
+    unMuffled = false,
+    unMufflable = false,
+    direction = 'left',
+  }) => ({
+    opacity: muffled && !unMuffled ? 0.5 : 1,
+    fontSize: muffled ? 16 : 20,
+    transformOrigin: `top ${direction}`,
+    transition: 'opacity 200ms, font 200ms',
+    '&:hover': {
+      opacity: unMufflable ? 1 : undefined,
+    },
+  }),
+)
