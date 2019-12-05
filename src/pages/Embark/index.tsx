5df424ce278041e3f1bffd48424857c4ffe9638b
Sam Pettersson <sam@sampettersson.com>
2019-12-05 11:07:44 +0100

    Fix background

--- src/pages/Embark/index.tsx
+++ src/pages/Embark/index.tsx
@@ -7,6 +7,7 @@ import {
 } from '@hedviginsurance/embark'
 import * as React from 'react'
 import { useHistory } from 'react-router'
+import { Global, css } from '@emotion/core'
 
 import { colorsV2 } from '@hedviginsurance/brand'
 import { StorageContainer } from '../../utils/StorageContainer'
@@ -15,30 +16,8 @@ import { resolveHouseInformation } from './houseInformation'
 import { resolvePersonalInformation } from './personalInformation'
 
 const EmbarkStyling = styled.div`
-  background-color: ${colorsV2.lightgray};
-  background: url('/new-member-assets/embark/orange-juice-mobile.png') no-repeat
-    center center fixed;
-  background-size: cover;
-
-  @media (min-width: 768px) {
-    background: url('/new-member-assets/embark/orange-juice.png') no-repeat
-      center center fixed;
-    background-size: cover;
-  }
-
-  @media (min-width: 1280px) {
-    background: url('/new-member-assets/embark/orange-juice@2x.png') no-repeat
-      center center fixed;
-    background-size: cover;
-  }
-
-  @media (min-width: 1920px) {
-    background: url('/new-member-assets/embark/orange-juice@3x.png') no-repeat
-      center center fixed;
-    background-size: cover;
-  }
-
   height: 100vh;
+  background-color: ${colorsV2.gray};
 
   * {
     margin: 0;
@@ -51,6 +30,36 @@ const EmbarkStyling = styled.div`
   li {
     list-style-type: none;
   }
+`
+
+const EmbarkBackgroundContainer = styled.picture<{
+  backgroundHasLoaded: boolean
+}>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  transition: opacity 1s;
+  transition-delay: 150ms;
+  ${(props) => `opacity: ${props.backgroundHasLoaded ? 1 : 0}`};
+`
+
+const EmbarkBackground = styled.img`
+  postion: fixed;
+  min-width: 100%;
+  min-height: 100%;
+  height: auto;
+  width: auto;
+  object-fit: cover;
+`
+
+const PassageContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
 `
 
 interface EmbarkProps {
@@ -101,39 +110,67 @@ const Embark: React.FunctionComponent<EmbarkProps> = (props) => {
     )
   }, [currentPassage])
 
+  const [backgroundHasLoaded, setBackgroundHasLoaded] = React.useState(false)
+
   return (
     <EmbarkStyling>
-      <StorageContainer>
-        {({ session }) => (
-          <Header
-            partnerName={
-              (session &&
-                session.getSession() &&
-                session.getSession()!.partner) ||
-              null
-            }
-            passage={currentPassage}
-            storyData={state.data}
-          />
-        )}
-      </StorageContainer>
-      <Passage
-        canGoBack={state.history.length > 1}
-        historyGoBackListener={(goBack) =>
-          history.listen((_: any, action: string) => {
-            if (action === 'POP' && state.history.length > 1) {
-              goBack()
-            }
-          })
-        }
-        passage={currentPassage}
-        goBack={() => {
-          dispatch({
-            type: 'GO_BACK',
-          })
-        }}
-        changePassage={goTo}
+      <Global
+        styles={css`
+          body {
+            overflow: hidden;
+          }
+        `}
       />
+      <EmbarkBackgroundContainer backgroundHasLoaded={backgroundHasLoaded}>
+        <EmbarkBackground
+          onLoad={() => setBackgroundHasLoaded(true)}
+          sizes="(max-width: 1400px) 100vw, 1400px"
+          srcSet="
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_200.jpg 200w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_481.jpg 481w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_677.jpg 677w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_858.jpg 858w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1013.jpg 1013w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1161.jpg 1161w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1296.jpg 1296w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1393.jpg 1393w,
+/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1400.jpg 1400w"
+          src="/new-member-assets/embark/orange-juice-copy_3x_xybd1d_c_scale,w_1400.jpg"
+        />
+      </EmbarkBackgroundContainer>
+      <PassageContainer>
+        <StorageContainer>
+          {({ session }) => (
+            <Header
+              partnerName={
+                (session &&
+                  session.getSession() &&
+                  session.getSession()!.partner) ||
+                null
+              }
+              passage={currentPassage}
+              storyData={state.data}
+            />
+          )}
+        </StorageContainer>
+        <Passage
+          canGoBack={state.history.length > 1}
+          historyGoBackListener={(goBack) =>
+            history.listen((_: any, action: string) => {
+              if (action === 'POP' && state.history.length > 1) {
+                goBack()
+              }
+            })
+          }
+          passage={currentPassage}
+          goBack={() => {
+            dispatch({
+              type: 'GO_BACK',
+            })
+          }}
+          changePassage={goTo}
+        />
+      </PassageContainer>
     </EmbarkStyling>
   )
 }
