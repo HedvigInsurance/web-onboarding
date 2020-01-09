3a9ed745cd2a7a0371c11d573927232203d6d06a
AlexSegerby <40593350+AlexSegerby@users.noreply.github.com>
2020-01-09 14:59:37 +0100

    Illness while travelling shortened

--- src/pages/OfferNew/Perils/perilData/en/house.tsx
+++ src/pages/OfferNew/Perils/perilData/en/house.tsx
@@ -238,7 +238,7 @@ export const housePerils: Peril[] = [
     icon: <Alarm />,
   },
   {
-    title: 'Illness and accidents while travelling',
+    title: 'Travel illness',
     description:
       'Our travel protection is eligible during the first 45 days of your trip and will reimburse you for costs due to acute illness, injury and acute dental injury. If considered necessery, we provide you with a flight back home to Sweden for further medical assistance.',
     covered: [
