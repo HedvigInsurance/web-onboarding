a305255c9483867da7b196a5f4873338619f4915
Sam Pettersson <sam@sampettersson.com>
2020-03-16 11:08:31 +0100

    Send embark store changed events to segment

--- src/pages/Embark/index.tsx
+++ src/pages/Embark/index.tsx
@@ -297,6 +297,14 @@ export const EmbarkRoot: React.FunctionComponent<EmbarkRootProps> = (props) => {
                     }}
                     initialStore={initialStore}
                     onStoreChange={(store) => {
+                      const castedWindow = window as any
+                      if (castedWindow && castedWindow.analytics) {
+                        castedWindow.analytics.track(
+                          `Embark Store Changed - ${props.name!}`,
+                          store,
+                        )
+                      }
+
                       window.sessionStorage.setItem(
                         `embark-store-${encodeURIComponent(props.name!)}`,
                         JSON.stringify(store),
