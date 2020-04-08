5cc4fa07e776544049ad044bfcb78469b6fee411
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-04-08 13:56:56 +0200

    Code cleanup

--- src/utils/Intercom.tsx
+++ src/utils/Intercom.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 import { useRouteMatch } from 'react-router'
 import { useVariation, Variation } from 'utils/hooks/useVariation'
 
+const createIntercom = () => {
+  const script = `
+      window.intercomSettings = {
+        app_id: "ziqa7goa"
+      };
+
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ziqa7goa';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    `
+  const scriptTag = window.document.createElement('script')
+  scriptTag.id = 'intercom-script'
+  scriptTag.nonce = window.document.querySelector<HTMLScriptElement>(
+    '[nonce]',
+  )?.nonce
+  scriptTag.innerHTML = script
+  window.document.body.append(scriptTag)
+}
+
 export const Intercom: React.FC = () => {
   const variation = useVariation()
   const isIntercomMatch = useRouteMatch(
@@ -12,31 +29,13 @@ export const Intercom: React.FC = () => {
       return
     }
 
-    const elementById = window.document.getElementById('intercom-script')
-    if (isIntercomMatch && !elementById) {
-      const script = `
-      window.intercomSettings = {
-        app_id: "ziqa7goa"
-      };
-
-      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ziqa7goa';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-    `
-      const scriptTag = window.document.createElement('script')
-      scriptTag.id = 'intercom-script'
-      scriptTag.nonce = window.document.querySelector<HTMLScriptElement>(
-        '[nonce]',
-      )?.nonce
-      scriptTag.innerHTML = script
-      window.document.body.append(scriptTag)
-    } else if (
-      isIntercomMatch &&
-      typeof (window as any).Intercom !== 'undefined'
-    ) {
+    const hasIntercomScript = window.document.getElementById('intercom-script')
+    const hasIntercomInstalled = typeof (window as any).Intercom !== 'undefined'
+    if (isIntercomMatch && !hasIntercomScript) {
+      createIntercom()
+    } else if (isIntercomMatch && hasIntercomInstalled) {
       ;(window as any).Intercom('boot')
-    } else if (
-      !isIntercomMatch &&
-      typeof (window as any).Intercom !== 'undefined'
-    ) {
+    } else if (!isIntercomMatch && hasIntercomInstalled) {
       ;(window as any).Intercom('shutdown')
     }
   }, [isIntercomMatch?.path])
