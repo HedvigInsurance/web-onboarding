9952d67508551140129ef04ec950eaa86cbf9db2
Gustav <gustav.een@tink.se>
2020-06-17 11:29:02 +0200

    Add Info text and adjust layout

--- src/pages/Forever/components/RedeemCode.tsx
+++ src/pages/Forever/components/RedeemCode.tsx
@@ -7,6 +7,8 @@ import { Form, Formik } from 'formik'
 import React from 'react'
 import { useTextKeys } from 'utils/hooks/useTextKeys'
 
+const INPUT_MAX_WIDTH = '21rem'
+
 interface RedeemCodeProps {
   code?: string
   currentLocale: string
@@ -17,6 +19,8 @@ const PageWrapper = styled.div`
   flex-direction: column;
   justify-content: space-between;
   height: 100vh;
+  padding-left: 1.625rem;
+  padding-right: 1.625rem;
   color: ${colorsV3.gray500};
   background-color: ${colorsV3.gray900};
 `
@@ -24,31 +28,32 @@ const PageWrapper = styled.div`
 const Header = styled.header`
   display: flex;
   justify-content: center;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  padding-top: 1.25rem;
+  padding-bottom: 1.15rem;
 `
 
-const Main = styled.div`
-  padding: 0 1.625rem;
-`
+const Main = styled.div``
 
 const CodeField = styled.div`
   margin: 0 auto;
 
   @media (min-width: 480px) {
-    max-width: 325px;
+    max-width: ${INPUT_MAX_WIDTH};
   }
 
   input {
     padding-right: 0;
     text-align: center;
+    text-transform: uppercase;
   }
 `
 
 const Footer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  padding: 1rem 1.625rem 2.5rem;
+  align-items: center;
+  padding: 1rem 0 2.5rem;
 `
 
 const LogoLink = styled.a`
@@ -59,16 +64,30 @@ const Paragraph = styled.p`
   text-align: center;
   color: ${colorsV3.gray500};
   font-size: 1rem;
-  margin-left: 1rem;
-  margin-right: 1rem;
 `
+
 const SubmitButton = styled(Button)`
   width: 100%;
   min-width: 12rem;
   height: 3.5rem;
+  margin-bottom: 1rem;
+  font-size: 1rem;
 
   @media (min-width: 480px) {
     width: auto;
+  }
+`
+
+const Info = styled.p`
+  max-width: 25rem;
+  margin-top: 0;
+  font-size: 0.6875rem;
+  color: ${colorsV3.gray500};
+  text-align: center;
+  line-height: 1.2;
+
+  a {
+    color: ${colorsV3.gray100};
   }
 `
 
@@ -120,6 +139,11 @@ export const RedeemCode: React.FC<RedeemCodeProps> = ({
         >
           {textKeys.FOREVER_LANDINGPAGE_BTN_LABEL()}
         </SubmitButton>
+        <Info
+          dangerouslySetInnerHTML={{
+            __html: textKeys.FOREVER_LANDINGPAGE_INFO_TEXT(),
+          }}
+        />
       </Footer>
     </PageWrapper>
   )
