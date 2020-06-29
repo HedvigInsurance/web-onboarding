37f88282bb4ff6012eb34f1739e6ffb3fa24297d
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-06-29 15:51:38 +0200

    Put back forgotten useHistory

--- src/pages/Forever/components/Intro/index.tsx
+++ src/pages/Forever/components/Intro/index.tsx
@@ -1,10 +1,12 @@
 import { LOCALE_PATH_PATTERN } from 'components/utils/CurrentLocale'
 import React from 'react'
+import { useHistory } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
 import { IntroStories } from './IntroStories'
 import { PreOnboardingScreen } from './PreOnboardingScreen'
 
 export const Intro: React.FC = () => {
+  const history = useHistory()
   return (
     <>
       <Switch>
