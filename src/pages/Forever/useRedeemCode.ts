2de82fd5ee9a45ec0771a96778a7b3749b488ee6
Gustav <gustav.een@tink.se>
2020-06-30 12:27:47 +0200

    Use switch statement

--- src/pages/Forever/useRedeemCode.ts
+++ src/pages/Forever/useRedeemCode.ts
@@ -20,55 +20,43 @@ export const useRedeemCode = () => {
         return
       }
 
-      if (result.data?.redeemCodeV2.__typename === 'SuccessfulRedeemResult') {
-        // TODO redirect to correct success screens
-        history.push(`/${locale}/new-member`)
-        return
-      }
-      if (result.data?.redeemCodeV2.__typename === 'CannotRedeemOwnCampaign') {
-        actions.setErrors({ code: 'FOREVER_ERROR_CANNOT_REDEEM_OWN_CAMPAIGN' })
-        return
-      }
-      if (
-        result.data?.redeemCodeV2.__typename ===
-        'CampaignCannotBeCombinedWithExisting'
-      ) {
-        actions.setErrors({
-          code: 'FOREVER_ERROR_CAMPAIGN_CANNOT_BE_COMBINED_WITH_EXISTING',
-        })
-        return
-      }
-      if (result.data?.redeemCodeV2.__typename === 'CampaignHasExpired') {
-        actions.setErrors({ code: 'FOREVER_ERROR_CAMPAIGN_HAS_EXPIRED' })
-        return
-      }
-      if (
-        result.data?.redeemCodeV2.__typename ===
-        'MemberIsNotEligibleForCampaign'
-      ) {
-        actions.setErrors({
-          code: 'FOREVER_ERROR_CAMPAIGN_CANNOT_BE_COMBINED_WITH_EXISTING',
-        })
-        return
-      }
-
-      if (result.data?.redeemCodeV2.__typename === 'CampaignDoesNotExist') {
-        actions.setErrors({ code: 'FOREVER_ERROR_CAMPAIGN_DOES_NOT_EXIST' })
-        return
-      }
-
-      if (
-        result.data?.redeemCodeV2.__typename ===
-        'CannotRedeemCampaignFromDifferentMarket'
-      ) {
-        actions.setErrors({
-          code: 'FOREVER_ERROR_CANNOT_REDEEM_CAMPAIGN_FROM_DIFFERENT_MARKET',
-        })
-        return
+      switch (result.data?.redeemCodeV2.__typename) {
+        case 'SuccessfulRedeemResult':
+          history.push(`/${locale}/intro`)
+          return
+        case 'CannotRedeemOwnCampaign':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_CANNOT_REDEEM_OWN_CAMPAIGN',
+          })
+          return
+        case 'CampaignCannotBeCombinedWithExisting':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_CAMPAIGN_CANNOT_BE_COMBINED_WITH_EXISTING',
+          })
+          return
+        case 'CampaignHasExpired':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_CAMPAIGN_HAS_EXPIRED',
+          })
+          return
+        case 'MemberIsNotEligibleForCampaign':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_MEMBER_IS_NOT_ELIGIBLE_FOR_CAMPAIGN',
+          })
+          return
+        case 'CampaignDoesNotExist':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_CAMPAIGN_DOES_NOT_EXIST',
+          })
+          return
+        case 'CannotRedeemCampaignFromDifferentMarket':
+          actions.setErrors({
+            code: 'FOREVER_ERROR_CANNOT_REDEEM_CAMPAIGN_FROM_DIFFERENT_MARKET',
+          })
+          return
       }
 
       if (result.errors && result.errors.length > 0) {
-        // TODO handle errors
         actions.setErrors({ code: 'FOREVER_ERROR_GENERIC' })
         return
       }
