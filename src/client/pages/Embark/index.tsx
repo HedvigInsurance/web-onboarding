fc97bcba3143680e84f15a7bc88172f882496657
Ellen Styrélius <ellen@hedvig.com>
2020-10-27 15:22:54 +0100

    Change the dependency array of useEffect in /Embark/index.tsx to contain the properties from 'currentPassage' that it uses instead of just 'currentPassage'.

--- src/client/pages/Embark/index.tsx
+++ src/client/pages/Embark/index.tsx
@@ -120,7 +120,16 @@ const Embark: React.FunctionComponent<EmbarkProps> = (props) => {
         embarkPassageName: props.name,
       })
     }
-  }, [currentPassage, history, state, props.baseUrl, props.name])
+  }, [
+    currentPassage.id,
+    currentPassage.url,
+    currentPassage.api,
+    currentPassage.externalRedirect,
+    history,
+    state,
+    props.baseUrl,
+    props.name,
+  ])
 
   const variation = useVariation()
 
