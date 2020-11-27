1e7009f0dca00836eb14095bfbbfdac3799118ed
Ellen Styrélius <ellen@hedvig.com>
2020-11-27 11:16:14 +0100

    Render <Sign> outside ScrollWrapper instead of inside of it. Add SignComponentSpacing styled component so that Sign won't cover content in the bottom of InnerWrapper.

--- src/client/pages/OfferNew/Checkout/index.tsx
+++ src/client/pages/OfferNew/Checkout/index.tsx
@@ -83,6 +83,10 @@ const ScrollWrapper = styled('div')<ScrollWrapperProps>`
   overflow-y: scroll;
 `
 
+const SignComponentSpacing = styled('div')`
+  height: 112px;
+`
+
 const InnerWrapper = styled('div')`
   display: flex;
   flex-direction: column;
@@ -318,21 +322,22 @@ export const Checkout: React.FC<Props> = ({
 
             <SignDisclaimer offerData={offerData} />
           </InnerWrapper>
-          <Sign
-            canInitiateSign={
-              canInitiateSign && !ssnUpdateLoading && !emailUpdateLoading
-            }
-            signUiState={signUiState}
-            signStatus={signStatus}
-            isLoading={
-              signQuotesMutation.loading ||
-              signUiState === SignUiState.STARTED ||
-              signUiState === SignUiState.STARTED_WITH_REDIRECT ||
-              emailUpdateLoading
-            }
-            onSignStart={startSign}
-          />
+          <SignComponentSpacing />
         </ScrollWrapper>
+        <Sign
+          canInitiateSign={
+            canInitiateSign && !ssnUpdateLoading && !emailUpdateLoading
+          }
+          signUiState={signUiState}
+          signStatus={signStatus}
+          isLoading={
+            signQuotesMutation.loading ||
+            signUiState === SignUiState.STARTED ||
+            signUiState === SignUiState.STARTED_WITH_REDIRECT ||
+            emailUpdateLoading
+          }
+          onSignStart={startSign}
+        />
       </OuterWrapper>
 
       <Backdrop visibilityState={visibilityState} onClick={onClose} />
