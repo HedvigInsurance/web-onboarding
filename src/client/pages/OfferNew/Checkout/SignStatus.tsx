da070110b4f736e81765069e044fb6c4e3b4c488
Ellen Styrélius <ellen@hedvig.com>
2020-12-14 22:06:44 +0100

    Rename isChatLinkVisible state, to hasSigningError, in SignStatus.tsx. Add a li'l WarningIcon by the text if that state is true.

--- src/client/pages/OfferNew/Checkout/SignStatus.tsx
+++ src/client/pages/OfferNew/Checkout/SignStatus.tsx
@@ -4,14 +4,33 @@ import { motion } from 'framer-motion'
 import { colorsV3 } from '@hedviginsurance/brand/dist'
 import { SignState, SignStatus as GraphQLSignStatus } from 'data/graphql'
 import { useTextKeys } from 'utils/textKeys'
+import { WarningIcon } from '../../../components/icons/Warning'
 import { SignUiState } from './Sign'
 import { LinkToChat } from './LinkToChat'
 
 const SignStatusWrapper = styled(motion.div)`
-  padding: 2rem 0;
-  text-align: center;
-  color: ${colorsV3.gray900};
+  padding: 2rem 1rem;
 `
+
+const StatusText = styled.div`
+  color: ${colorsV3.gray700};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.875rem;
+  text-align: center;
+`
+
+const IconWrapper = styled.div`
+  padding: 0.25rem 0.5rem 0 0;
+  align-items: center;
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+`
+
 const BANK_ID_STATUS_TEXT_KEYS: Record<string, string> = {
   started: 'SIGN_BANKID_CODE_STARTED',
   userSign: 'SIGN_BANKID_CODE_USER_SIGN',
@@ -30,7 +49,7 @@ type Props = {
 }
 
 export const SignStatus: React.FC<Props> = ({ signStatus, signUiState }) => {
-  const [isChatLinkVisible, setIsChatLinkVisible] = useState(false)
+  const [hasSigningError, setHasSigningError] = useState(false)
   const [signStatusTextKey, setSignStatusTextKey] = useState<string | null>(
     null,
   )
@@ -39,7 +58,7 @@ export const SignStatus: React.FC<Props> = ({ signStatus, signUiState }) => {
   useEffect(() => {
     if (signUiState === 'FAILED') {
       setSignStatusTextKey('CHECKOUT_SIGN_GENERIC_ERROR')
-      setIsChatLinkVisible(true)
+      setHasSigningError(true)
     }
     if (signUiState === 'STARTED_WITH_REDIRECT') {
       setSignStatusTextKey('CHECKOUT_SIGN_STARTED_WITH_REDIRECT')
@@ -58,19 +77,30 @@ export const SignStatus: React.FC<Props> = ({ signStatus, signUiState }) => {
         setSignStatusTextKey(BANK_ID_STATUS_TEXT_KEYS[statusCode])
       }
       if (signState === SignState.Failed) {
-        setIsChatLinkVisible(true)
+        setHasSigningError(true)
       }
     }
   }, [signStatus])
 
-  return signStatusTextKey ? (
-    <SignStatusWrapper
-      initial={{ height: 0, opacity: 0 }}
-      animate={{ height: 'auto', opacity: 1 }}
-      transition={{ type: 'spring', stiffness: 400, damping: 100 }}
-    >
-      {textKeys[signStatusTextKey]()}
-      {isChatLinkVisible && <LinkToChat />}
-    </SignStatusWrapper>
-  ) : null
+  return (
+    <>
+      {signStatusTextKey && (
+        <SignStatusWrapper
+          initial={{ height: 0, opacity: 0 }}
+          animate={{ height: 'auto', opacity: 1 }}
+          transition={{ type: 'spring', stiffness: 400, damping: 100 }}
+        >
+          <StatusText>
+            {hasSigningError && (
+              <IconWrapper>
+                <WarningIcon color={colorsV3.gray700} />
+              </IconWrapper>
+            )}
+            {textKeys[signStatusTextKey]()}
+          </StatusText>
+          {hasSigningError && <LinkToChat />}
+        </SignStatusWrapper>
+      )}
+    </>
+  )
 }
