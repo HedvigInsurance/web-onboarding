3828459850f9be50f084d6ccea89de8082e5bc2f
viwi <victor@growbotics.se>
2021-01-15 10:09:18 +0100

    Added to mediaqueries

--- src/client/utils/mediaQueries.ts
+++ src/client/utils/mediaQueries.ts
@@ -1,1 +1,5 @@
+export const MS_BP = 800
+export const MOBILE_BP = 450
 export const LARGE_SCREEN_MEDIA_QUERY = '@media screen and (min-width: 1000px)'
+export const MEDIUM_SCREEN_MEDIA_QUERY = `@media (max-width: ${MS_BP}px)`
+export const MOBILE_SCREEN_MEDIA_QUERY = `@media (max-width: ${MOBILE_BP}px)`
