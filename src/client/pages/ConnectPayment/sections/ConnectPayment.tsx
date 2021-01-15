cbbd8a1f79a9f1653edd7509f5979f4d6e6bdb8f
viwi <victor@growbotics.se>
2021-01-15 10:41:33 +0100

    Updated connect payment

--- src/client/pages/ConnectPayment/sections/ConnectPayment.tsx
+++ src/client/pages/ConnectPayment/sections/ConnectPayment.tsx
@@ -5,105 +5,136 @@ import { Market, useMarket } from 'components/utils/CurrentLocale'
 import { AdyenCheckout } from 'pages/ConnectPayment/components/AdyenCheckout'
 import { TrustlyCheckout } from 'pages/ConnectPayment/components/TrustlyCheckout'
 import { useTextKeys } from 'utils/textKeys'
+import {
+  MOBILE_SCREEN_MEDIA_QUERY,
+  MEDIUM_SCREEN_MEDIA_QUERY,
+} from 'utils/mediaQueries'
 import { useVariation, Variation } from 'utils/hooks/useVariation'
 import { AVYWindow } from 'utils/tracking/signing'
 import { ErrorModal } from '../components/ErrorModal'
 
 const SITEWRAPPER = 1300
-const BP = 800
-const MOBILE = 450
 
-const Background = styled('div')({
-  position: 'fixed',
-  top: 0,
-  right: 0,
-  bottom: 0,
-  left: 0,
-  backgroundColor: colorsV3.gray900,
-  zIndex: -1,
-})
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: ${colorsV3.gray900};
+  z-index: -1;
+`
 
-const InnerWrapper = styled('div')({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  maxWidth: SITEWRAPPER,
-  margin: 'auto',
-  paddingLeft: '10px',
-  paddingRight: '10px',
-  paddingTop: '25vh',
-  color: colorsV3.gray500,
-  [`@media (max-width: ${BP}px)`]: {
-    maxWidth: '100%',
-    flexDirection: 'column',
-    paddingTop: 40 + 70,
+const InnerWrapper = styled.div`
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  flex-direction: row;
+  align-items: flex-start;
+  max-width: ${SITEWRAPPER};
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 7.25rem;
+  color: ${colorsV3.gray500};
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    max-width: 100%;
+    padding-top: 6.75rem;
+    flex-direction: column-reverse;
+    width: 100%;
+  }
+`
+
+const TextColumn = styled.div`
+  width: 60%;
+  padding-bottom: 2.5rem;
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    width: 100%;
+  }
+`
+
+const ImageColumn = styled.div`
+  width: 40%;
+  margin-top: 0.5rem;
+  padding-left: 2rem;
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    width: 100%;
+    padding: 0;
+    text-align: center;
   },
-})
+}
+`
 
-const TextColumn = styled('div')({
-  width: '60%',
-  paddingRight: 20,
-  paddingLeft: 20,
-  paddingBottom: 40,
+const Header = styled.h1`
+  margin-top: 0;
+  margin-bottom: 2.25rem;
+  font-size: 3rem;
+  line-height: 3.75rem;
+  color: ${colorsV3.white};
+  ${MOBILE_SCREEN_MEDIA_QUERY} {
+    text-align: center;
+    margin-bottom: 2rem;
+    font-size: 2rem;
+    line-height: 2rem;
+  }
+`
 
-  [`@media (max-width: ${BP}px)`]: {
-    width: '100%',
-    textAlign: 'center',
-  },
-})
+type HeaderPartProps = {
+  addWhiteSpace?: boolean
+}
+const HeaderPart = styled.span<HeaderPartProps>`
+  display: block;
+  font-family: ${fonts.FAVORIT};
+  font-weight: 400;
 
-const ImageColumn = styled('div')({
-  width: '40%',
-  paddingRight: 20,
-  paddingLeft: 20,
-  paddingBottom: 40,
+  ${MOBILE_SCREEN_MEDIA_QUERY} {
+    display: initial;
+    ${(props) =>
+      props.addWhiteSpace &&
+      `
+      :before {
+        content:'\\00a0';
+      }`}
+`
+const ConnectText = styled.div`
+  width: 65%;
+  margin-bottom: 1rem;
+  font-size: 1.25rem;
+  color: ${colorsV3.white};
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    width: 100%;
+  }
+  ${MOBILE_SCREEN_MEDIA_QUERY} {
+    font-size: 1rem;
+  }
+`
+const InstructionWrapper = styled(ConnectText)`
+  margin-bottom: 1rem;
+  color: ${colorsV3.gray500};
+`
 
-  [`@media (max-width: ${BP}px)`]: {
-    width: '100%',
-    textAlign: 'center',
-  },
-})
+const ConnectPaymentImage = styled.img`
+  margin-left: auto;
+  width: 80%;
+  margin-right: auto;
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    width: 40%;
+    margin-bottom: 2rem;
+  }
+`
 
-const Header = styled('h1')({
-  marginTop: '0px',
-  marginBottom: '30px',
-  fontSize: '56px',
-  lineHeight: '60px',
-  color: colorsV3.white,
-  [`@media (max-width: ${MOBILE}px)`]: {
-    fontSize: '44px',
-    lineHeight: '52px',
-  },
-})
-
-const HeaderPart = styled('span')({
-  display: 'block',
-  fontFamily: fonts.FAVORIT,
-  fontWeight: 400,
-})
-
-const ConnectText = styled('div')({
-  width: '65%',
-  marginBottom: '45px',
-  [`@media (max-width: ${BP}px)`]: {
-    width: '100%',
-  },
-})
-
-const ConnectPaymentImage = styled('img')({
-  marginBottom: '30px',
-  marginTop: '30px',
-  marginLeft: 'auto',
-  width: '100%',
-  [`@media (max-width: ${BP}px)`]: {
-    marginRight: 'auto',
-    width: '80%',
-  },
-  [`@media (max-width: ${MOBILE}px)`]: {
-    width: '100%',
-  },
-})
+const InstructionsList = styled.ol`
+  list-style: none;
+  padding: 0;
+  counter-reset: styled-number;
+`
+const InstructionItem = styled.li`
+  counter-increment: styled-number;
+  :: before {
+    content: counter(styled-number) '. ';
+  }
+`
 
 export const ConnectPaymentPage: React.FC = () => {
   const textKeys = useTextKeys()
@@ -130,9 +161,21 @@ export const ConnectPaymentPage: React.FC = () => {
             <HeaderPart>
               {textKeys.ONBOARDING_CONNECT_DD_PRE_HEADLINE()}
             </HeaderPart>
-            <HeaderPart>{textKeys.ONBOARDING_CONNECT_DD_HEADLINE()}</HeaderPart>
+            <HeaderPart addWhiteSpace>
+              {textKeys.ONBOARDING_CONNECT_DD_HEADLINE()}
+            </HeaderPart>
           </Header>
           <ConnectText>{textKeys.ONBOARDING_CONNECT_DD_BODY()}</ConnectText>
+          <InstructionWrapper>
+            <InstructionsList>
+              <InstructionItem>
+                {textKeys.ONBOARDING_CONNECT_DD_INSTRUCTIONS_CONNECT_PAYMENT()}
+              </InstructionItem>
+              <InstructionItem>
+                {textKeys.ONBOARDING_CONNECT_DD_INSTRUCTIONS_DOWNLOAD_APP()}
+              </InstructionItem>
+            </InstructionsList>
+          </InstructionWrapper>
           {market === Market.Se && <TrustlyCheckout onSuccess={onSuccess} />}
           {(market === Market.No || market === Market.Dk) && (
             <AdyenCheckout onSuccess={onSuccess} />
