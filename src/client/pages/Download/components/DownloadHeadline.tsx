40b73d3eb498ac6f04945cf90bae52147b2de997
Ellen Styrélius <ellen@hedvig.com>
2021-01-27 17:58:18 +0100

    Make the entire 'Headline' in DownloadHeadline.tsx an 'h1' instead of 'div'.

--- src/client/pages/Download/components/DownloadHeadline.tsx
+++ src/client/pages/Download/components/DownloadHeadline.tsx
@@ -4,7 +4,7 @@ import { colorsV3 } from '@hedviginsurance/brand'
 import { useTextKeys } from 'utils/textKeys'
 import { LARGE_SCREEN_MEDIA_QUERY } from 'utils/mediaQueries'
 
-const Headline = styled.div`
+const Headline = styled.h1`
   width: 100%;
   margin-bottom: 3rem;
   font-size: 2rem;
@@ -22,20 +22,12 @@ const Headline = styled.div`
   }
 `
 
-const MainHeadline = styled.h1`
-  margin: 0;
-  font-size: inherit;
-  display: inline;
-`
-
 export const DownloadHeadline: React.FC = () => {
   const textKeys = useTextKeys()
 
   return (
     <Headline>
-      <MainHeadline>
-        {textKeys.ONBOARDING_DOWNLOAD_HEADLINE_PART_1() + ' '}
-      </MainHeadline>
+      <span>{textKeys.ONBOARDING_DOWNLOAD_HEADLINE_PART_1() + ' '}</span>
       <span>{textKeys.ONBOARDING_DOWNLOAD_HEADLINE_PART_2()}</span>
     </Headline>
   )
