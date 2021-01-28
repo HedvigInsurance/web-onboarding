13a21d803d5b0710f1c45291f6986d407effe111
gustaveen <gustaveen@gmail.com>
2021-01-28 20:49:12 +0100

    Remove code splitting

--- src/client/pages/LoginApp/index.tsx
+++ src/client/pages/LoginApp/index.tsx
@@ -1,23 +1,8 @@
 import React from 'react'
 import { Market, useMarket } from 'components/utils/CurrentLocale'
-import { LoadingPage } from 'components/LoadingPage'
-
-const LoginAppScreen = React.lazy(() =>
-  import(/* webpackChunkName: "debugger" */ './LoginAppScreen').then((m) => ({
-    default: m.LoginAppScreen,
-  })),
-)
+import { LoginAppScreen } from './LoginAppScreen'
 
 export const LoginApp: React.FC = () => {
   const market = useMarket()
-
-  if (market !== Market.No) {
-    return null
-  }
-
-  return (
-    <React.Suspense fallback={<LoadingPage centeredTopBar loading />}>
-      <LoginAppScreen />
-    </React.Suspense>
-  )
+  return market === Market.No ? <LoginAppScreen /> : null
 }
