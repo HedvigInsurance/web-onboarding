db1ab46e8fe3d935028f47be4a7a22e0b2cd2b63
Ellen Styrélius <ellen@hedvig.com>
2021-03-22 12:38:13 +0100

    Separate LanguagePicker tests from eachother to make it clearer what's wrong when they fail. Update outdated snapshots.

--- src/client/pages/Embark/LanguagePicker.test.tsx
+++ src/client/pages/Embark/LanguagePicker.test.tsx
@@ -7,48 +7,60 @@ import { LanguagePicker } from './LanguagePicker'
 
 jest.mock('../../apolloClient', () => ({}))
 
-it('renders SE without 💥', () => {
+describe('Swedish language picker renders without 💥', () => {
   const wrapper = mount(
     <MemoryRouter initialEntries={['/se/new-member']}>
       <LanguagePicker />
     </MemoryRouter>,
   )
 
-  expect(wrapper.find('Link[to*="/se/new-member"]')).toHaveLength(1)
-  expect(wrapper.find('Link[to*="/se-en/new-member"]')).toHaveLength(1)
-  expect(wrapper.find('Link[to*="/no/new-member"]')).toHaveLength(0)
-  expect(wrapper.find('Link[to*="/no-en/new-member"]')).toHaveLength(0)
+  it('has the correct links', () => {
+    expect(wrapper.find('Link[to*="/se/new-member"]')).toHaveLength(1)
+    expect(wrapper.find('Link[to*="/se-en/new-member"]')).toHaveLength(1)
+    expect(wrapper.find('Link[to*="/no/new-member"]')).toHaveLength(0)
+    expect(wrapper.find('Link[to*="/no-en/new-member"]')).toHaveLength(0)
+  })
 
-  expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  it('matches snaphot', () => {
+    expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  })
 })
 
-it('renders NO without 💥', () => {
+describe('Norwegian language picker renders without 💥', () => {
   const wrapper = mount(
     <MemoryRouter initialEntries={['/no/new-member']}>
       <LanguagePicker />
     </MemoryRouter>,
   )
 
-  expect(wrapper.find('Link[to*="/no/new-member"]')).toHaveLength(1)
-  expect(wrapper.find('Link[to*="/no-en/new-member"]')).toHaveLength(1)
-  expect(wrapper.find('Link[to*="/se/new-member"]')).toHaveLength(0)
-  expect(wrapper.find('Link[to*="/se-en/new-member"]')).toHaveLength(0)
+  it('has the correct links', () => {
+    expect(wrapper.find('Link[to*="/no/new-member"]')).toHaveLength(1)
+    expect(wrapper.find('Link[to*="/no-en/new-member"]')).toHaveLength(1)
+    expect(wrapper.find('Link[to*="/se/new-member"]')).toHaveLength(0)
+    expect(wrapper.find('Link[to*="/se-en/new-member"]')).toHaveLength(0)
+  })
 
-  expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  it('matches snaphot', () => {
+    expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  })
 })
 
-it('renders DK without 💥', () => {
+describe('Danish language picker renders without 💥', () => {
   const wrapper = mount(
     <MemoryRouter initialEntries={['/dk/new-member']}>
       <LanguagePicker />
     </MemoryRouter>,
   )
 
-  expect(wrapper.find('Link')).toHaveLength(2)
-  expect(wrapper.find('Link[to*="/dk/new-member"]')).toHaveLength(1)
-  expect(wrapper.find('Link[to*="/dk-en/new-member"]')).toHaveLength(1)
+  it('has the correct links', () => {
+    expect(wrapper.find('Link')).toHaveLength(2)
+    expect(wrapper.find('Link[to*="/dk/new-member"]')).toHaveLength(1)
+    expect(wrapper.find('Link[to*="/dk-en/new-member"]')).toHaveLength(1)
+  })
 
-  expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  it('matches snaphot', () => {
+    expect(toJson(wrapper.find(LanguagePicker))).toMatchSnapshot()
+  })
 })
 
 it('opens when clicking icon', () => {
