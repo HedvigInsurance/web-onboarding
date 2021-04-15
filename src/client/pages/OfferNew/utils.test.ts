32c610c93ad5f904ff1cd2b57695270f447365c7
Ellen Styrélius <ellen@hedvig.com>
2021-04-15 17:25:51 +0200

    Add some tests for the 'getMainQuote' function, and create a 'noTravel' object in offerDataMock.ts to use in one of the tests.

--- src/client/pages/OfferNew/utils.test.ts
+++ src/client/pages/OfferNew/utils.test.ts
@@ -1,9 +1,23 @@
 import {
+  DanishHomeContentsDetails,
+  NorwegianHomeContentsDetails,
+} from 'src/client/data/graphql'
+import {
+  seApartementBrf,
   noCombo,
   noComboYouth,
+  noTravel,
   dkHomeContentOwn,
+  dkHomeContentAccidentTravel,
 } from 'utils/testData/offerDataMock'
-import { isBundle, isNorwegian, isSwedish, isDanish, isYouth } from './utils'
+import {
+  isBundle,
+  isNorwegian,
+  isSwedish,
+  isDanish,
+  isYouth,
+  getMainQuote,
+} from './utils'
 
 describe('quote validation', () => {
   it('validates if quote is norwegian', () => {
@@ -21,9 +35,42 @@ describe('quote validation', () => {
     expect(isYouth(noCombo)).toBe(false)
     expect(isYouth(noComboYouth)).toBe(true)
   })
+
   it('validates if quote is danish', () => {
     expect(isDanish(dkHomeContentOwn)).toBe(true)
     expect(isNorwegian(dkHomeContentOwn)).toBe(false)
     expect(isSwedish(dkHomeContentOwn)).toBe(false)
+  })
+})
+
+describe('getMainQuote function', () => {
+  it('returns the one quote if there is just one quote in the quote array', () => {
+    const swedishBrfQuote = seApartementBrf.quotes[0]
+    const mainSwedishBrfQuote = getMainQuote(seApartementBrf)
+    expect(mainSwedishBrfQuote.id).toBe(swedishBrfQuote.id)
+
+    const norwegianTravelQuote = noTravel.quotes[0]
+    const mainTravelQuote = getMainQuote(noTravel)
+    expect(mainTravelQuote.id).toBe(norwegianTravelQuote.id)
+
+    const danishHomeContentsQuote = dkHomeContentOwn.quotes[0]
+    const mainHomeContentsQuote = getMainQuote(dkHomeContentOwn)
+    expect(mainHomeContentsQuote.id).toBe(danishHomeContentsQuote.id)
+  })
+
+  it('returns the Home Contents quote from Norwegian quote bundle', () => {
+    const mainQuote = getMainQuote(noCombo)
+    expect(mainQuote.contractType).toContain('HOME_CONTENT')
+    expect(
+      (mainQuote.quoteDetails as NorwegianHomeContentsDetails).street,
+    ).toBeTruthy()
+  })
+
+  it('returns the Home Contents quote from Danish Home-Accident-Travel bundle', () => {
+    const mainQuote = getMainQuote(dkHomeContentAccidentTravel)
+    expect(mainQuote.contractType).toContain('HOME_CONTENT')
+    expect(
+      (mainQuote.quoteDetails as DanishHomeContentsDetails).street,
+    ).toBeTruthy()
   })
 })
