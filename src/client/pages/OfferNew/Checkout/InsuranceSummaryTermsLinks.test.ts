bf97de8b86084edcf205cf6530483daefe340a12
Ellen Styrélius <ellen@hedvig.com>
2021-04-20 10:25:30 +0200

    Fix and add more tests to InsuranceSummaryTermsLinks.test.ts: - Add test that makes sure Privacy Policy is not part of the returned terms array. - Fix test checking for duplicates so that it does its thing on both mocked bundle and on mocked Swedish apartment quote. - Fix test making sure all terms from bundle exist in the returned array to take into account if there would be a Privacy Policy among the quote InsuranceTerms (it currently isn't but it should work if it were to be added).

--- src/client/pages/OfferNew/Checkout/InsuranceSummaryTermsLinks.test.ts
+++ src/client/pages/OfferNew/Checkout/InsuranceSummaryTermsLinks.test.ts
@@ -1,63 +1,137 @@
 import {
   noCombo,
+  seApartementBrf,
   insuranceTermsNoHomeContentsMock,
   insuranceTermsNoTravelMock,
-} from '../../../utils/testData/offerDataMock'
+} from 'utils/testData/offerDataMock'
+import { InsuranceTerm, InsuranceTermType } from 'data/graphql'
 import { getInsuranceTerms } from './InsuranceSummaryTermsLinks'
 
+const getPrivacyPolicyTerms = (terms: Terms) => {
+  return terms.filter(
+    ({ termType }) => termType === InsuranceTermType.PrivacyPolicy,
+  )
+}
+
+const getDuplicates = (terms: Terms) => {
+  return terms.filter(({ data }) => {
+    const { displayName, url } = data
+    const occurences = terms.filter(
+      ({ data }) => data.displayName === displayName && data.url === url,
+    )
+
+    return occurences.length > 1
+  })
+}
+
+type Terms = {
+  termType: InsuranceTermType
+  data: InsuranceTerm
+}[]
+
 describe('getInsuranceTerms function', () => {
+  const termsCombo = getInsuranceTerms({ offerData: noCombo })
+  const termsSwedishApartment = getInsuranceTerms({
+    offerData: seApartementBrf,
+  })
+
+  it('returns array of insurance terms objects excluding privacy policy', () => {
+    const privacyPolicyComboTerms = getPrivacyPolicyTerms(termsCombo)
+    const privacyPolicySwedishApartmentTerms = getPrivacyPolicyTerms(
+      termsSwedishApartment,
+    )
+
+    expect(privacyPolicyComboTerms).toHaveLength(0)
+    expect(privacyPolicySwedishApartmentTerms).toHaveLength(0)
+  })
+
+  it('returns array of insurance terms objects without duplicates', () => {
+    const duplicatesInBundleTerms = getDuplicates(termsCombo)
+    const duplicatesInSingleQuoteTerms = getDuplicates(termsSwedishApartment)
+
+    expect(duplicatesInBundleTerms).toHaveLength(0)
+    expect(duplicatesInSingleQuoteTerms).toHaveLength(0)
+  })
+
   describe('with bundle quotes', () => {
-    const termsCombo = getInsuranceTerms({ offerData: noCombo })
-    const termsComboDisplayNames = termsCombo.map(({ data }) => {
-      return data.displayName
-    })
-    const termsComboUrls = termsCombo.map(({ data }) => {
-      return data.url
-    })
-
-    it('returns an array of insurance terms objects where all insurance terms from every quotes exist', () => {
-      const termsNoTravel = [...insuranceTermsNoTravelMock].map(
-        ([termType, data]) => {
-          return { termType, data }
-        },
-      )
-      const termsNoHomeContents = [...insuranceTermsNoHomeContentsMock].map(
-        ([termType, data]) => {
-          return { termType, data }
-        },
-      )
-      const homeContentsTermsMissingFromComboTerms = termsNoHomeContents.filter(
-        ({ data }) => {
-          return (
-            !termsComboDisplayNames.includes(data.displayName) ||
-            !termsComboUrls.includes(data.url)
-          )
-        },
-      )
-      const travelTermsMissingFromComboTerms = termsNoTravel.filter(
-        ({ data }) => {
-          return (
-            !termsComboDisplayNames.includes(data.displayName) ||
-            !termsComboUrls.includes(data.url)
-          )
-        },
-      )
-      expect(homeContentsTermsMissingFromComboTerms).toHaveLength(0)
-      expect(travelTermsMissingFromComboTerms).toHaveLength(0)
-    })
-
-    it('returns an array of insurance terms objects without duplicates', () => {
-      const duplicates = termsCombo.filter(({ data }) => {
-        const { displayName, url } = data
-
-        const occurences = termsCombo.filter(({ data }) => {
-          return data.displayName === displayName && data.url === url
-        })
-
-        return occurences.length > 1
+    it('returns array where every insurance term from all quotes exist, excluding privacy policy', () => {
+      const termsComboDisplayNames = termsCombo.map(({ data }) => {
+        return data.displayName
+      })
+      const termsComboUrls = termsCombo.map(({ data }) => {
+        return data.url
       })
 
-      expect(duplicates).toHaveLength(0)
+      const termsNoTravelMock = [...insuranceTermsNoTravelMock].map(
+        ([termType, data]) => {
+          return { termType, data }
+        },
+      )
+      const termsNoHomeContentsMock = [...insuranceTermsNoHomeContentsMock].map(
+        ([termType, data]) => {
+          return { termType, data }
+        },
+      )
+
+      const homeContentsTermsMissingFromComboTerms = termsNoHomeContentsMock.filter(
+        ({ data }) => {
+          return (
+            !termsComboDisplayNames.includes(data.displayName) ||
+            !termsComboUrls.includes(data.url)
+          )
+        },
+      )
+      const travelTermsMissingFromComboTerms = termsNoTravelMock.filter(
+        ({ data }) => {
+          return (
+            !termsComboDisplayNames.includes(data.displayName) ||
+            !termsComboUrls.includes(data.url)
+          )
+        },
+      )
+
+      const hasPrivacyPolicyInHomeContentTerms = insuranceTermsNoHomeContentsMock.has(
+        InsuranceTermType.PrivacyPolicy,
+      )
+      const hasPrivacyPolicyInTravelTerms = insuranceTermsNoTravelMock.has(
+        InsuranceTermType.PrivacyPolicy,
+      )
+      const privacyPolicyInHomeContentTerms = homeContentsTermsMissingFromComboTerms.find(
+        ({ termType }) => termType === InsuranceTermType.PrivacyPolicy,
+      )
+      const privacyPolicyInTravelTerms = travelTermsMissingFromComboTerms.find(
+        ({ termType }) => termType === InsuranceTermType.PrivacyPolicy,
+      )
+
+      expect(homeContentsTermsMissingFromComboTerms).toHaveLength(
+        hasPrivacyPolicyInHomeContentTerms ? 1 : 0,
+      )
+      expect(travelTermsMissingFromComboTerms).toHaveLength(
+        hasPrivacyPolicyInTravelTerms ? 1 : 0,
+      )
+      expect(privacyPolicyInHomeContentTerms?.termType).toBe(
+        hasPrivacyPolicyInHomeContentTerms
+          ? InsuranceTermType.PrivacyPolicy
+          : undefined,
+      )
+      expect(privacyPolicyInTravelTerms?.termType).toBe(
+        hasPrivacyPolicyInTravelTerms
+          ? InsuranceTermType.PrivacyPolicy
+          : undefined,
+      )
+    })
+  })
+
+  describe('with single quote', () => {
+    it('returns array that has the same length as the size of "insuranceTerms" property on quote', () => {
+      const hasPrivacyPolicy = seApartementBrf.quotes[0].insuranceTerms.has(
+        InsuranceTermType.PrivacyPolicy,
+      )
+      const sizeOfTermsInQuote = seApartementBrf.quotes[0].insuranceTerms.size
+
+      expect(termsSwedishApartment).toHaveLength(
+        hasPrivacyPolicy ? sizeOfTermsInQuote - 1 : sizeOfTermsInQuote,
+      )
     })
   })
 })
