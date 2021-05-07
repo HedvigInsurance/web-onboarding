3c26b314ff074db13fc00f78569f1b11d956993b
Robin Andeer <robin.andeer@gmail.com>
2021-05-07 09:50:24 +0200

    Add support for danish in Forever language picker (#525)
    Centralize language options by market.
    Dynamically render language options on Forever page.

--- src/client/pages/Embark/languagePickerData.ts
+++ src/client/pages/Embark/languagePickerData.ts
@@ -7,24 +7,56 @@ const LANGUAGE_TITLES = {
   danish: 'Dansk',
 }
 
+const SHORT_LANGUAGE_TITLES = {
+  english: 'En',
+  swedish: 'Sv',
+  norwegian: 'No',
+  danish: 'Da',
+}
+
 type Options = {
   linkTo: LocalePath
   title: string
+  shortTitle: string
 }[]
 
 type LanguagePickerData = Record<Market, Options>
 
 export const languagePickerData: LanguagePickerData = {
   SE: [
-    { linkTo: 'se', title: LANGUAGE_TITLES.swedish },
-    { linkTo: 'se-en', title: LANGUAGE_TITLES.english },
+    {
+      linkTo: 'se',
+      title: LANGUAGE_TITLES.swedish,
+      shortTitle: SHORT_LANGUAGE_TITLES.swedish,
+    },
+    {
+      linkTo: 'se-en',
+      title: LANGUAGE_TITLES.english,
+      shortTitle: SHORT_LANGUAGE_TITLES.english,
+    },
   ],
   NO: [
-    { linkTo: 'no', title: LANGUAGE_TITLES.norwegian },
-    { linkTo: 'no-en', title: LANGUAGE_TITLES.english },
+    {
+      linkTo: 'no',
+      title: LANGUAGE_TITLES.norwegian,
+      shortTitle: SHORT_LANGUAGE_TITLES.norwegian,
+    },
+    {
+      linkTo: 'no-en',
+      title: LANGUAGE_TITLES.english,
+      shortTitle: SHORT_LANGUAGE_TITLES.english,
+    },
   ],
   DK: [
-    { linkTo: 'dk', title: LANGUAGE_TITLES.danish },
-    { linkTo: 'dk-en', title: LANGUAGE_TITLES.english },
+    {
+      linkTo: 'dk',
+      title: LANGUAGE_TITLES.danish,
+      shortTitle: SHORT_LANGUAGE_TITLES.danish,
+    },
+    {
+      linkTo: 'dk-en',
+      title: LANGUAGE_TITLES.english,
+      shortTitle: SHORT_LANGUAGE_TITLES.english,
+    },
   ],
 }
--- src/client/pages/Forever/components/LanguagePicker.tsx
+++ src/client/pages/Forever/components/LanguagePicker.tsx
@@ -3,7 +3,8 @@ import { colorsV3 } from '@hedviginsurance/brand'
 import React from 'react'
 import { useParams } from 'react-router'
 import { NavLink } from 'react-router-dom'
-import { Market, useMarket } from 'components/utils/CurrentLocale'
+import { useMarket } from 'components/utils/CurrentLocale'
+import { languagePickerData } from '../../Embark/languagePickerData'
 
 const LanguagePickerWrapper = styled.div`
   display: flex;
@@ -35,20 +36,16 @@ export const LanguagePicker: React.FC = () => {
   const urlParams: { locale: string; code?: string } = useParams()
   const code = urlParams.code ? urlParams.code : ''
 
-  if (market === Market.No) {
-    return (
-      <LanguagePickerWrapper>
-        <Language to={`/no/forever/${code}`}>No</Language>
-        <Divider />
-        <Language to={`/no-en/forever/${code}`}>En</Language>
-      </LanguagePickerWrapper>
-    )
-  }
   return (
     <LanguagePickerWrapper>
-      <Language to={`/se/forever/${code}`}>Sv</Language>
-      <Divider />
-      <Language to={`/se-en/forever/${code}`}>En</Language>
+      {languagePickerData[market].map((item, index) => (
+        <React.Fragment key={item.linkTo}>
+          {index !== 0 && <Divider />}
+          <Language to={`/${item.linkTo}/forever/${code}`}>
+            {item.shortTitle}
+          </Language>
+        </React.Fragment>
+      ))}
     </LanguagePickerWrapper>
   )
 }
