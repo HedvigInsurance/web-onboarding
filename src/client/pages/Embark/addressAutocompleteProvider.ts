62dcc5bc87a2170ccce3d8e46e0bb6a6ebdbc237
Robin Andeer <robin@hedvig.com>
2021-07-02 13:50:55 +0200

    Update imports from Embark

--- src/client/pages/Embark/addressAutocompleteProvider.ts
+++ src/client/pages/Embark/addressAutocompleteProvider.ts
@@ -1,18 +1,6 @@
 import { gql } from '@apollo/client'
-import { AddressSuggestion } from '@hedviginsurance/embark'
-// import {
-//   AddressSuggestion,
-//   AddressAutocompleteType,
-//   AddressAutocompleteQuery
-// } from '@hedviginsurance/embark'
+import { AddressAutocompleteQuery } from '@hedviginsurance/embark'
 import { apolloClient } from 'apolloClient'
-
-// TODO: remove in favor of imports from embark
-type AddressAutocompleteType = 'STREET' | 'BUILDING' | 'APARTMENT'
-type AddressAutocompleteQuery = (
-  searchTerm: string,
-  options?: { type: AddressAutocompleteType },
-) => Promise<AddressSuggestion[]>
 
 const query = gql`
   query AutoComplete($term: String!, $type: AddressAutocompleteType!) {
