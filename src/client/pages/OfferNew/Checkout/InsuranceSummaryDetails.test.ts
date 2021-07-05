88e218308e73da86a9ff7897793d3c1443ab5250
Ellen Styrélius <ellen@hedvig.com>
2021-07-05 15:47:10 +0200

    Add a few unit tests for the 'getAddress' function in new file InsuranceSummaryDetails.test.ts

---
+++ src/client/pages/OfferNew/Checkout/InsuranceSummaryDetails.test.ts
@@ -0,0 +1,75 @@
+import {
+  noCombo as mockedOfferDataNoCombo,
+  seApartementBrf as mockedOfferDataSeApartment,
+  dkHomeContentAccidentTravel as mockedOfferDataDkHomeAccidentTravel,
+} from 'utils/testData/offerDataMock'
+import { getAddress } from './InsuranceSummaryDetails'
+
+describe('getAddress function', () => {
+  describe('with Swedish quote', () => {
+    it('only returns street name and street number when quote is Swedish', () => {
+      const swedishApartmentQuoteDetails =
+        mockedOfferDataSeApartment.quotes[0].quoteDetails
+
+      const address = getAddress(swedishApartmentQuoteDetails)
+      expect(address).toBe('Testgatan 23')
+    })
+  })
+
+  describe('with Norwegian quotes', () => {
+    it('only returns street name and street number when quote is Norwegian Home Contents quote', () => {
+      const norwegianHomeContentsQuote = mockedOfferDataNoCombo.quotes[1]
+
+      const norwegianHomeContentsQuoteDetails =
+        norwegianHomeContentsQuote.quoteDetails
+
+      const address = getAddress(norwegianHomeContentsQuoteDetails)
+      expect(address).toBe('Testveien 23')
+    })
+
+    it('returns null when quote is Norwegian Travel quote', () => {
+      const norwegianTravelQuote = mockedOfferDataNoCombo.quotes[0]
+
+      const norwegianTravelQuoteDetails = norwegianTravelQuote.quoteDetails
+
+      const address = getAddress(norwegianTravelQuoteDetails)
+      expect(address).toBe(null)
+    })
+  })
+
+  describe('with Danish quotes', () => {
+    const danishHomeContentsQuote =
+      mockedOfferDataDkHomeAccidentTravel.quotes[0]
+
+    it('returns floor and apartment in addition to street name and street number when those properties exist', () => {
+      const danishHomeContentsQuoteDetails =
+        danishHomeContentsQuote.quoteDetails
+
+      const address = getAddress(danishHomeContentsQuoteDetails)
+      expect(address).toBe('Theodore Roosevelts Vej 1, 2. tv')
+    })
+
+    it('only returns street name and street number when no other properties exist', () => {
+      const danishHomeContentsQuoteDetailsWithoutFloorAndApartment = {
+        ...danishHomeContentsQuote.quoteDetails,
+        floor: null,
+        apartment: null,
+      }
+
+      const address = getAddress(
+        danishHomeContentsQuoteDetailsWithoutFloorAndApartment,
+      )
+      expect(address).toBe('Theodore Roosevelts Vej 1')
+    })
+
+    it('returns street name, street number and floor when apartment property does not exist', () => {
+      const danishHomeContentsQuoteDetailsWithoutApartment = {
+        ...danishHomeContentsQuote.quoteDetails,
+        apartment: null,
+      }
+
+      const address = getAddress(danishHomeContentsQuoteDetailsWithoutApartment)
+      expect(address).toBe('Theodore Roosevelts Vej 1, 2.')
+    })
+  })
+})
