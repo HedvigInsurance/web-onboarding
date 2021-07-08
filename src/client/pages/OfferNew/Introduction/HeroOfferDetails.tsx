6b1e284add29b021fb0cc7281ed6f81b6e481bf8
Ellen Styrélius <ellen@hedvig.com>
2021-07-08 11:59:18 +0200

    Remove unused imports in HeroOfferDetails.tsx

--- src/client/pages/OfferNew/Introduction/HeroOfferDetails.tsx
+++ src/client/pages/OfferNew/Introduction/HeroOfferDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
 import { OfferData } from 'pages/OfferNew/types'
