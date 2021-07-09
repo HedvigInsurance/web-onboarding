eac49eaaa85d2749ca0f59029b1627fbc27d552a
Ellen Styrélius <ellen@hedvig.com>
2021-07-09 17:06:46 +0200

    Make insurance type headlines larger in PerilRow.tsx

--- src/client/pages/OfferNew/Perils/PerilRow.tsx
+++ src/client/pages/OfferNew/Perils/PerilRow.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import { InsuranceValues } from 'pages/OfferNew/Perils/InsuranceValues'
 import { PerilCollection } from 'pages/OfferNew/Perils/PerilCollection'
 import { PerilModal } from 'pages/OfferNew/Perils/PerilModal'
 import { OfferQuote } from 'pages/OfferNew/types'
+import { LARGE_SCREEN_MEDIA_QUERY } from 'utils/mediaQueries'
+
+const InsuranceTypeHeadline = styled.div`
+  padding: 16px 0;
+  font-size: 1.375rem;
+
+  ${LARGE_SCREEN_MEDIA_QUERY} {
+    font-size: 1.5rem;
+  }
+`
 
 type Props = {
   offerQuote: OfferQuote
@@ -14,7 +25,7 @@ export const PerilRow: React.FC<Props> = ({ offerQuote }) => {
 
   return (
     <>
-      <h3>{offerQuote.displayName}</h3>
+      <InsuranceTypeHeadline>{offerQuote.displayName}</InsuranceTypeHeadline>
       <PerilCollection
         perils={offerQuote.perils}
         setCurrentPeril={setCurrentPeril}
