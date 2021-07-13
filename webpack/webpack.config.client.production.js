149dcc80b02c58f3d38f7d28f9f894c84c302289
Robin Andeer <robin@hedvig.com>
2021-07-13 15:07:51 +0200

    Upload source maps to Sentry from webpack
    Add sentry webpack plugin.
    Remove thread loader (doesn't work with sentry webpack plugin).
    Conditionally include Sentry webpack plugin if sentry auth token is defined.

--- webpack/webpack.config.client.production.js
+++ webpack/webpack.config.client.production.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const { StatsWriterPlugin } = require('webpack-stats-plugin')
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 const webpackConfig = require('./webpack.config.base')
 
 const root = path.resolve(__dirname, '..')
@@ -41,6 +42,16 @@ module.exports = webpackConfig({
       process: 'process/browser',
     }),
     new StatsWriterPlugin({ filename: 'stats.json' }),
+    process.env.SENTRY_AUTH_TOKEN
+      ? new SentryWebpackPlugin({
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          org: 'hedvig',
+          project: 'web-onboarding',
+          release: process.env.HEROKU_SLUG_COMMIT,
+          include: './build/new-member-assets',
+          ignore: ['node_modules', 'webpack.*.js'],
+        })
+      : null,
     // new BundleAnalyzerPlugin(),
   ].filter(Boolean),
 })
