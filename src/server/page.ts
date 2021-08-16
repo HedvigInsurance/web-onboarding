c3a2c54a99c843d898c32d6693a3324fbf5636ff
Gustav Eén <gustaveen@gmail.com>
2021-08-16 11:33:42 +0200

    Add data-nonce to GTM script This enables us to pass on the nonce to Custom HTML tags in GTM

--- src/server/page.ts
+++ src/server/page.ts
@@ -74,7 +74,7 @@ const template = (
     </script>
 
     <!-- Google Tag Manager -->
-    <script nonce="${cspNonce}">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script id="gtmScript" nonce="${cspNonce}" data-nonce="${cspNonce}">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;j.setAttribute('nonce','${cspNonce}');f.parentNode.insertBefore(j,f);
