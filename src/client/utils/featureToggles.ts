be9f9ef44216164c47100985c77477187f41fcaf
Meletios Mazarakiotis <39048374+meletios@users.noreply.github.com>
2021-10-14 13:48:38 +0200

    Revert "Disable credit checks temporarily (#617)" (#622)
    This reverts commit 77efc8f89640c75879529ac3372dcfd60a867350.

--- src/client/utils/featureToggles.ts
+++ src/client/utils/featureToggles.ts
@@ -2,8 +2,6 @@ import { Market, useMarket } from '../components/utils/CurrentLocale'
 
 export const useCreditCheckInfo = () => {
   const market = useMarket()
-  return (
-    [Market.No].includes(market) &&
-    window.hedvigClientConfig.appEnvironment !== 'production'
-  )
+
+  return [Market.No].includes(market)
 }
