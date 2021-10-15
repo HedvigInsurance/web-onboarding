c57d459c9050b84fe45c9def203508d04ecf41da
Meletios Mazarakiotis <39048374+meletios@users.noreply.github.com>
2021-10-15 08:43:23 +0200

    Revert "Disable credit checks in prod (#626)"
    This reverts commit 5430a29637fbe6475e3c7894f9f638a6dd1779be.

--- src/client/utils/featureToggles.ts
+++ src/client/utils/featureToggles.ts
@@ -3,8 +3,5 @@ import { Market, useMarket } from '../components/utils/CurrentLocale'
 export const useCreditCheckInfo = () => {
   const market = useMarket()
 
-  return (
-    [Market.No].includes(market) &&
-    window.hedvigClientConfig.appEnvironment !== 'production'
-  )
+  return [Market.No].includes(market)
 }
