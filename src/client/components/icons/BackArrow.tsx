f1a936a1c62d404f55b82b11b307bbf29a43b001
Amanda Flodström <amanda@hedvig.com>
2021-11-08 15:56:36 +0100

    Wrap Cross, ForwardArrow and BackArrow in IconRoot

--- src/client/components/icons/BackArrow.tsx
+++ src/client/components/icons/BackArrow.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import { IconRoot } from './IconRoot'
 
 export const BackArrow = () => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <IconRoot>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M9.06066 12L16.5303 4.53033L15.4697 3.46967L6.93934 12L15.4697 20.5303L16.5303 19.4697L9.06066 12Z"
       fill="#777777"
     />
-  </svg>
+  </IconRoot>
 )
--- src/client/components/icons/ForwardArrow.tsx
+++ src/client/components/icons/ForwardArrow.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import { IconRoot } from './IconRoot'
 
 export const ForwardArrow = () => (
-  <svg
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <IconRoot>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M14.9393 12L7.46967 4.53033L8.53033 3.46967L17.0607 12L8.53032 20.5303L7.46966 19.4697L14.9393 12Z"
       fill="#777777"
     />
-  </svg>
+  </IconRoot>
 )
