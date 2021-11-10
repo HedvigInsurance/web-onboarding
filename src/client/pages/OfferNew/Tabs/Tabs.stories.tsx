3dac6eb4d3e958f500ef2bdc26a09682eadbea0c
Robin Andeer <robin.andeer@gmail.com>
2021-11-10 11:29:04 +0100

    Implement tab navigation on offer page (#652)
    * Implement tab navigation on offer page
    
    Update tabs storybook story.
    Fix underline animation implementation. Add underline background element inside tabs header.
    
    Co-authored-by: Robin Andeer <robin.andeer@gmail.com>

--- src/client/pages/OfferNew/Tabs/Tabs.stories.tsx
+++ src/client/pages/OfferNew/Tabs/Tabs.stories.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import { Tabs } from './Tabs'
+
 const mockInsurances = [
   {
     id: '1',
     name: 'Hemförsäkring',
-    selected: true,
     content: 'one',
   },
   {
     id: '2',
     name: 'Olycksfallsförsäkring',
-    selected: false,
     content: 'two ',
   },
   {
     id: '3',
     name: 'Inbo',
-    selected: false,
     content: 'three',
   },
 ]
@@ -30,12 +29,5 @@ export default {
 }
 
 export const Default = () => {
-  const [insurances, setInsurances] = React.useState(mockInsurances)
-
-  const handleOnChange = (id: string) =>
-    setInsurances((prev) =>
-      prev.map((item) => ({ ...item, selected: item.id === id })),
-    )
-
-  return <Tabs items={insurances} onChange={handleOnChange} />
+  return <Tabs items={mockInsurances} onChange={action('change tab')} />
 }
