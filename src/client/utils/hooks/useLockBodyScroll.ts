a7b33564765b047e7c5401620009374e09e2cc5d
Guilherme Popolin <guilherme@hedvig.com>
2021-11-11 11:50:23 +0100

    Revert "refactor(useBodyLockScroll): updated to a simplier implementation"
    This reverts commit 864a143fcb77d38dedd10ab1082079f8284a1c9a.

--- src/client/utils/hooks/useLockBodyScroll.ts
+++ src/client/utils/hooks/useLockBodyScroll.ts
@@ -4,7 +4,8 @@ type UseLockBodyScrollConfig = { lock: boolean }
 
 export const useLockBodyScroll = ({ lock = true }: UseLockBodyScrollConfig) => {
   useLayoutEffect(() => {
-    const previousOverflowStyle = document.body.style.overflow
+    const previousOverflowStyle = window.getComputedStyle(document.body)
+      .overflow
 
     if (lock) {
       document.body.style.overflow = 'hidden'
