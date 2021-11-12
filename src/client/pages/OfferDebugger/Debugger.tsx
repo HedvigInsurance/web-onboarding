35f2af8826079cc27f9cb94c143b7fcbe30ae4f3
Robin Andeer <robin@hedvig.com>
2021-11-12 17:16:04 +0100

    Remove custom hook

--- src/client/pages/OfferDebugger/Debugger.tsx
+++ src/client/pages/OfferDebugger/Debugger.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
-import { FetchResult } from '@apollo/client'
 import { Button } from 'components/buttons'
-import {
-  CreateOnboardingSessionMutation,
-  useCreateOnboardingSessionMutation,
-} from 'data/graphql'
+import { useCreateOnboardingSessionMutation } from 'data/graphql'
 import { useMarket } from 'components/utils/CurrentLocale'
 import { useCurrentLocale } from 'l10n/useCurrentLocale'
 import { QuoteData } from './components/QuoteData'
@@ -22,30 +18,17 @@ const Row = styled.div`
   padding-bottom: 1rem;
 `
 
-type SessionResult = FetchResult<CreateOnboardingSessionMutation>
-
-const useOnboardingSession = () => {
+export const Debugger: React.FC = () => {
   const market = useMarket()
   const locale = useCurrentLocale().isoLocale
-  const [createOnboardingSession] = useCreateOnboardingSessionMutation()
-  const [result, setResult] = React.useState<SessionResult>()
-
-  const create = async () => {
-    const result = await createOnboardingSession({
-      variables: { country: market, locale },
-    })
-    setResult(result)
-  }
-
-  return { create, result }
-}
-
-export const Debugger: React.FC = () => {
-  const session = useOnboardingSession()
-  const sessionId = session.result?.data?.onboardingSession_create
+  const [
+    createOnboardingSession,
+    { data },
+  ] = useCreateOnboardingSessionMutation()
+  const sessionId = data?.onboardingSession_create
 
   const handleClickNewSession = async () => {
-    await session.create()
+    await createOnboardingSession({ variables: { country: market, locale } })
   }
 
   return (
