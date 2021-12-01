bd630397c5d5f156ec069b90244baf291d27c2dd
Guilherme S. Popolin <guilherme@hedvig.com>
2021-12-01 09:59:26 +0100

    GRW-400 - Feature: Added UpsellCard for Accident Sweden (#693)
    * feature(UpsellCard): added new component
    
    * feature(UpsellCard): added UpsellCard for accident swedish
    
    * refactor(Checkout): better props naming
    
    * refactor(Checkout): make usage of l10n/useCurrentLocale hook
    
    * refactor(Checkout): updated text keys for upsell card
    
    * chore(translations): added latest translations
    
    * refactor(UpsellCard): updated styles ands text keys
    
    * fix(Checout stories): updated due changes on Checkout API
    
    * style(UpsellCard): change its exibition order in Checkout sidebar
    
    * GRW-400 Use object instead of function + add feature flag + folder restructuring
    
    * GRW-400 Remove textKeys prop and use hook instead
    
    * GRW-400 Refactor Upsell Card component to avoid comment
    
    * GRW-400 Add GTM tracking for Upsell Card
    
    * GRW-400 Fix TS error
    
    * GRW-400 Renaming functions and props
    
    Co-authored-by: Mehdi <89857278+mehdielket@users.noreply.github.com>
    Co-authored-by: Mehdi <mehdi.elkettani@hedvig.com>

---
+++ src/client/pages/OfferNew/Checkout/UpsellCard/Card.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { colorsV3 } from '@hedviginsurance/brand'
+
+import { MEDIUM_SCREEN_MEDIA_QUERY } from 'utils/mediaQueries'
+
+import { Button } from 'components/buttons'
+
+const CardRoot = styled.div`
+  background-color: ${colorsV3.gray200};
+  border-radius: 8px;
+  padding: 1.5em;
+  margin-bottom: 1rem;
+
+  & > *:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 1.5rem;
+  ine-height: 1.33;
+
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    font-size: 1.5rem;
+  }
+`
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 0.875rem;
+
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    font-size: 1rem;
+  }
+`
+
+const Price = styled.h2`
+  font-size: 1.25rem;
+
+  ${MEDIUM_SCREEN_MEDIA_QUERY} {
+    font-size: 1.5rem;
+  }
+`
+
+const AddButton = styled(Button)`
+  font-size: 0.875rem;
+  padding: 0.875em 1.5em;
+`
+
+type UpsellCardProps = {
+  title: string
+  description: string
+  price: string
+  actionButtonLabel: string
+  onConfirm: () => void
+}
+
+export const Card = ({
+  title,
+  description,
+  price,
+  actionButtonLabel,
+  onConfirm,
+}: UpsellCardProps) => {
+  return (
+    <CardRoot>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <Price>{price}</Price>
+      <AddButton onClick={onConfirm}>{actionButtonLabel}</AddButton>
+    </CardRoot>
+  )
+}
