7e8cdd28cc030e35b6d05cf83ebeb0c8d9aeb864
amandaflod <59652803+amandaflod@users.noreply.github.com>
2021-12-14 14:04:43 +0100

    Quote Cart Api Forever page (#714)
    * Add QuoteCartForeverPage to ForeverPage
    
    * Add handleSubmit to QuoteCartForeverPage
    
    * Save campaign code in sessionStorage
    
    Co-author: @robinandeer

--- src/client/pages/Forever/index.tsx
+++ src/client/pages/Forever/index.tsx
@@ -3,18 +3,22 @@ import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
 import React, { useEffect } from 'react'
 import Helmet from 'react-helmet-async'
-import { RouteComponentProps } from 'react-router'
+import { RouteComponentProps, useHistory } from 'react-router'
 import { Route, Switch } from 'react-router-dom'
+import { FormikHelpers } from 'formik'
 import { HedvigLogo } from 'components/icons/HedvigLogo'
-import { getIsoLocale, useCurrentLocale } from 'components/utils/CurrentLocale'
 import { Page } from 'components/utils/Page'
 import { SessionContainer } from 'containers/SessionContainer'
-import { useUpdatePickedLocaleMutation } from 'data/graphql'
+import { useCampaignQuery, useUpdatePickedLocaleMutation } from 'data/graphql'
 import { Intro } from 'pages/Forever/components/Intro'
 import { localePathPattern } from 'l10n/localePathPattern'
 import { useTextKeys } from 'utils/textKeys'
 import { useStorage } from 'utils/StorageContainer'
-import { RedeemCode } from './components/RedeemCode'
+import { useCurrentLocale } from 'l10n/useCurrentLocale'
+import { Features, useFeature } from 'utils/hooks/useFeature'
+import { captureSentryError } from 'utils/sentry-client'
+import { CampaignCode } from 'utils/campaignCode'
+import { RedeemCode, RedeemCodeFormValue } from './components/RedeemCode'
 import { useRedeemCode } from './useRedeemCode'
 
 type ForeverProps = RouteComponentProps<{
@@ -53,17 +57,17 @@ const LogoLink = styled.a`
   color: ${colorsV3.gray100};
 `
 
-export const Forever: React.FC<ForeverProps> = ({
+export const OldForeverPage: React.FC<ForeverProps> = ({
   match: {
     params: { code },
   },
 }) => {
-  const currentLocale = useCurrentLocale()
+  const { isoLocale, path: pathLocale } = useCurrentLocale()
   const textKeys = useTextKeys()
   const { handleSubmit } = useRedeemCode()
   const [updatePickedLocale] = useUpdatePickedLocaleMutation({
     variables: {
-      pickedLocale: getIsoLocale(currentLocale),
+      pickedLocale: isoLocale,
     },
   })
   const storage = useStorage()
@@ -73,7 +77,7 @@ export const Forever: React.FC<ForeverProps> = ({
       return
     }
     updatePickedLocale()
-  }, [currentLocale, updatePickedLocale, hasToken])
+  }, [isoLocale, updatePickedLocale, hasToken])
 
   return (
     <>
@@ -94,7 +98,7 @@ export const Forever: React.FC<ForeverProps> = ({
             />
             <PageWrapper>
               <Header>
-                <LogoLink href={'/' + currentLocale}>
+                <LogoLink href={'/' + pathLocale}>
                   <HedvigLogo width={94} />
                 </LogoLink>
               </Header>
@@ -117,5 +121,85 @@ export const Forever: React.FC<ForeverProps> = ({
         )}
       </SessionContainer>
     </>
+  )
+}
+
+const QuoteCartForeverPage: React.FC<ForeverProps> = ({
+  match: {
+    params: { code },
+  },
+}) => {
+  const { path: pathLocale } = useCurrentLocale()
+  const textKeys = useTextKeys()
+  const history = useHistory()
+
+  const { refetch: fetchCampaign } = useCampaignQuery({ skip: true })
+
+  const handleSubmit = async (
+    form: RedeemCodeFormValue,
+    actions: FormikHelpers<RedeemCodeFormValue>,
+  ) => {
+    const { code } = form
+    try {
+      const result = await fetchCampaign({ code })
+      if (result.data.campaign.code) {
+        CampaignCode.save(code)
+        history.push({ pathname: `/${pathLocale}/new-member` })
+      } else {
+        actions.setErrors({ code: 'FOREVER_ERROR_GENERIC' })
+      }
+    } catch (error) {
+      captureSentryError(error)
+      actions.setErrors({ code: 'FOREVER_ERROR_GENERIC' })
+    }
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>{textKeys.FOREVER_LANDINGPAGE_TITLE()}</title>
+        <meta property="og:image" content="" />
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      <Page>
+        <Global
+          styles={css`
+            body {
+              background-color: ${colorsV3.gray900};
+            }
+          `}
+        />
+        <PageWrapper>
+          <Header>
+            <LogoLink href={'/' + pathLocale}>
+              <HedvigLogo width={94} />
+            </LogoLink>
+          </Header>
+
+          <Switch>
+            <Route
+              path={localePathPattern + '/forever/:code?'}
+              exact
+              render={() => (
+                <RedeemCode referralCode={code} onSubmit={handleSubmit} />
+              )}
+            />
+            <Route
+              path={localePathPattern + '/forever/:code/intro'}
+              component={Intro}
+            />
+          </Switch>
+        </PageWrapper>
+      </Page>
+    </>
+  )
+}
+
+export const Forever = (props: ForeverProps) => {
+  const [isUsingQuoteCartApi] = useFeature([Features.QUOTE_CART_API])
+  return isUsingQuoteCartApi ? (
+    <QuoteCartForeverPage {...props} />
+  ) : (
+    <OldForeverPage {...props} />
   )
 }
