b42e4170dae0e542ef717c43804c64a525d4bf20
Ellen Styrélius <ellen@hedvig.com>
2021-12-22 17:35:43 +0100

    Remove 'reactPageRoutes' from routes.tsx and use the new 'routes' array in App.tsx for the client-side routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import { Route, Switch, useHistory } from 'react-router-dom'
 import { TextKeyProvider } from 'utils/textKeys'
-import { reactPageRoutes } from '../routes'
+import { routes } from '../routes'
 import { AppTokenRetrieval } from './utils/AppTokenRetrieval'
 import { GlobalCss } from './utils/globalStyles'
 import { Intercom } from './utils/Intercom'
@@ -34,15 +34,21 @@ export const App: React.ComponentType<StorageState> = ({ session }) => {
           <StorageContext.Provider value={{ session }}>
             <AppTokenRetrieval>
               <Switch>
-                {reactPageRoutes.map(({ path, exact, Component, render }) => (
-                  <Route
-                    key={path}
-                    path={path}
-                    exact={exact}
-                    component={Component}
-                    render={render}
-                  />
-                ))}
+                {routes.map(({ path, clientRouteData }) => {
+                  if (!clientRouteData || !(typeof path === 'string')) {
+                    return null
+                  }
+                  const { exact, Component, render } = clientRouteData
+                  return (
+                    <Route
+                      key={path}
+                      path={path}
+                      exact={exact}
+                      component={Component}
+                      render={render}
+                    />
+                  )
+                })}
               </Switch>
             </AppTokenRetrieval>
           </StorageContext.Provider>
