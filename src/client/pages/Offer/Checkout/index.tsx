4cf1876c66a6313cb066aa60d32a31bc9990fa10
Guilherme Popolin <guilherme@hedvig.com>
2021-12-27 18:21:29 +0100

    feat(Checkout): make usage of CampaignBadge

--- src/client/pages/Offer/Checkout/index.tsx
+++ src/client/pages/Offer/Checkout/index.tsx
@@ -30,6 +30,7 @@ import { useLockBodyScroll } from 'utils/hooks/useLockBodyScroll'
 import { useFeature, Features } from 'utils/hooks/useFeature'
 import { useCurrentLocale } from 'l10n/useCurrentLocale'
 import { CloseButton } from 'components/CloseButton/CloseButton'
+import { CampaignBadge } from 'components/CampaignBadge/CampaignBadge'
 import { DiscountTag } from 'components/DiscountTag/DiscountTag'
 import { setupQuoteCartSession } from 'containers/SessionContainer'
 import { reportUnderwritingLimits } from 'utils/sentry-client'
@@ -120,7 +121,7 @@ const Section = styled.div`
   width: 100%;
 `
 
-const DiscountTagWrapper = styled.div`
+const StyledCampaignBadge = styled(CampaignBadge)`
   margin-top: 2rem;
 `
 
@@ -461,9 +462,12 @@ export const Checkout = ({
           <InnerWrapper>
             <CloseButton onClick={onClose} />
             <Section>
-              <DiscountTagWrapper>
+              <StyledCampaignBadge
+                quoteCartId={quoteCartId}
+                offerData={offerData}
+              >
                 <DiscountTag offerData={offerData} />
-              </DiscountTagWrapper>
+              </StyledCampaignBadge>
               <Heading>{textKeys.CHECKOUT_HEADING()}</Heading>
               <PriceBreakdown
                 offerData={offerData}
