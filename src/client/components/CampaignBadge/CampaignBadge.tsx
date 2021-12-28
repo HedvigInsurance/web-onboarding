4316e521ba351c3e770ff336db0f4d744f25f815
Guilherme S. Popolin <guilherme@hedvig.com>
2021-12-28 14:19:03 +0100

    refactor(GRW-689): propagate OfferNew/Checkout changes to Offer/Checkout (#727)
    * refactor(Badge): expose prop types
    
    * feat(CampaignBadge): added new component
    
    * feat(Sidebar): make usage of CampaignBadge
    
    * feat(Checkout): make usage of CampaignBadge
    
    * fix(CampaignBadge): removed undisired log
    
    * refactor(CampaignBadge): updated api

---
+++ src/client/components/CampaignBadge/CampaignBadge.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+import { Badge, BadgeProps } from 'components/Badge/Badge'
+
+import { useAppliedCampaignNameQuery } from 'data/graphql'
+import { useTextKeys } from 'utils/textKeys'
+import { useCurrentLocale } from 'l10n/useCurrentLocale'
+
+const CampaignBadgeStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  align-items: flex-start;
+
+  & > *:not(:last-child) {
+    margin-right: 0.5rem;
+  }
+`
+
+export type CampaignBadgeProps = Omit<BadgeProps, 'children'> & {
+  quoteCartId: string
+  isNorwegianBundle: boolean
+}
+
+export const CampaignBadge: React.FC<CampaignBadgeProps> = ({
+  className,
+  quoteCartId,
+  isNorwegianBundle,
+  ...others
+}) => {
+  const { isoLocale } = useCurrentLocale()
+  const textKeys = useTextKeys()
+
+  const { data: campaignData, loading } = useAppliedCampaignNameQuery({
+    variables: {
+      quoteCartId,
+      locale: isoLocale,
+    },
+  })
+  const campaignText = campaignData?.quoteCart.campaign?.displayValue ?? ''
+
+  if (loading || !campaignText) return null
+
+  const discounts: Array<React.ReactNode> = [
+    ...(isNorwegianBundle ? [textKeys.SIDEBAR_NO_BUNDLE_CAMPAIGN_TEXT()] : []),
+    ...(campaignText ? [campaignText] : []),
+  ]
+
+  return (
+    <CampaignBadgeStyled className={className}>
+      {discounts.map((text, index) => (
+        <Badge key={index} {...others}>
+          {text}
+        </Badge>
+      ))}
+    </CampaignBadgeStyled>
+  )
+}
