19d3cee7e623a73e615cd8a2cb820363a51477cc
Ellen Styrélius <ellen@hedvig.com>
2022-01-10 15:33:03 +0100

    Add a SubSection component

---
+++ src/client/components/CheckoutDetails/SubSection.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import { colorsV3 } from '@hedviginsurance/brand'
+
+const { white } = colorsV3
+
+export const SubSection = styled.div`
+  background-color: ${white};
+  width: 100%;
+  padding: 8px 16px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`
