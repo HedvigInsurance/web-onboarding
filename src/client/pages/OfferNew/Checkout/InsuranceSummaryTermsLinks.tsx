3efa253f8048a465917b518a2d93acd0ca8f790a
Amanda Flodström <amanda@hedvig.com>
2022-01-11 22:04:25 +0100

    Font size and padding adjustments

--- src/client/pages/OfferNew/Checkout/InsuranceSummaryTermsLinks.tsx
+++ src/client/pages/OfferNew/Checkout/InsuranceSummaryTermsLinks.tsx
@@ -9,12 +9,12 @@ import { Row } from './InsuranceSummary'
 const LinksWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0.5rem 0;
+  padding-bottom: 1.25rem;
 `
 
 const DisplayNameWrapper = styled.div`
   color: ${colorsV3.gray900};
-  font-size: 14px;
+  font-size: 0.875;
 `
 const LinkRow = styled.div`
   display: flex;
@@ -24,10 +24,10 @@ const LinkRow = styled.div`
 `
 
 const Link = styled(RawLink)`
-  font-size: 12px;
+  font-size: 0.75rem;
   color: ${colorsV3.gray700};
   text-decoration: none;
-  padding: 0.5rem 0.75rem 0 0;
+  padding: 0.5rem 1rem 0 0;
   :hover,
   :focus {
     color: ${colorsV3.gray900};
