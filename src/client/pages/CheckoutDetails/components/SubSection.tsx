7e0cd06c696706458435e38b6ad071fec4bb2a60
Ellen Styrélius <ellen@hedvig.com>
2022-01-13 16:39:24 +0100

    Move all the files in /components/CheckoutDetails/ folder to new folder /pages/CheckoutDetails/components/ and delete (now empty) /components/CheckoutDetails/ folder

--- src/client/pages/CheckoutDetails/components/SubSection.tsx
+++ src/client/pages/CheckoutDetails/components/SubSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
-import { Headline } from '../Headline/Headline'
+import { Headline } from '../../../components/Headline/Headline'
 
 const { white } = colorsV3
 
