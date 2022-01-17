df33eaefbd10073b836fa0dd949114eba79e10de
Ellen Styrélius <ellen@hedvig.com>
2022-01-17 11:48:01 +0100

    Replace px with rem in the styled components in SubSection.tsx

--- src/client/pages/CheckoutDetails/components/SubSection.tsx
+++ src/client/pages/CheckoutDetails/components/SubSection.tsx
@@ -12,12 +12,12 @@ type Props = {
 
 const Wrapper = styled.div`
   display: grid;
-  gap: 8px;
+  gap: 0.5rem;
 `
 const Card = styled.div`
   background-color: ${white};
   width: 100%;
-  padding: 8px 16px;
+  padding: 0.5rem 1rem;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
 `
