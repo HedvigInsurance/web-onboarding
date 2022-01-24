edfd960c19d7f41cec2672e02933b6a5595ca620
Ellen Styrélius <ellen@hedvig.com>
2022-01-24 16:15:24 +0100

    Replace arg 'translationsLocale' in Footer.stories.tsx with 'localePath' of type LocaleLabel. Use this for the MemoryRouter 'initialEntries' and add a switch statement to get the correct translations locale based on the 'localePath'

--- src/client/components/checkout/Footer.stories.tsx
+++ src/client/components/checkout/Footer.stories.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import { MemoryRouter } from 'react-router'
+import { LocaleLabel } from 'l10n/locales'
 import { TextKeyProvider, TranslationsLocale } from 'utils/textKeys'
 import { Footer, Props as FooterProps } from './Footer'
 
-type StoryProps = { translationsLocale: TranslationsLocale } & FooterProps
+type StoryProps = {
+  localePath: LocaleLabel
+} & FooterProps
 
 const storyMeta: Meta<StoryProps> = {
   title: 'Components/Checkout/Footer',
@@ -14,7 +17,7 @@ const storyMeta: Meta<StoryProps> = {
     buttonOnClick: () => {
       console.log('👉 Click!')
     },
-    translationsLocale: 'en_NO',
+    localePath: 'no-en',
   },
   parameters: {
     backgrounds: {
@@ -25,12 +28,37 @@ const storyMeta: Meta<StoryProps> = {
 
 export default storyMeta
 
-const Template: Story<StoryProps> = ({ translationsLocale, ...rest }) => (
-  <MemoryRouter initialEntries={['/se-en/new-member/checkout/details']}>
-    <TextKeyProvider locale={translationsLocale}>
-      <Footer {...rest} />
-    </TextKeyProvider>
-  </MemoryRouter>
-)
+const Template: Story<StoryProps> = ({ localePath, ...rest }) => {
+  const getTranslationsLocale = (
+    localePath: LocaleLabel,
+  ): TranslationsLocale => {
+    switch (localePath) {
+      case 'se':
+        return 'sv_SE'
+      case 'se-en':
+        return 'en_SE'
+      case 'no':
+        return 'nb_NO'
+      case 'no-en':
+        return 'en_NO'
+      case 'dk':
+        return 'da_DK'
+      case 'dk-en':
+        return 'en_DK'
+      default:
+        return 'en'
+    }
+  }
+
+  return (
+    <MemoryRouter
+      initialEntries={[`${localePath}/new-member/checkout/details`]}
+    >
+      <TextKeyProvider locale={getTranslationsLocale(localePath)}>
+        <Footer {...rest} />
+      </TextKeyProvider>
+    </MemoryRouter>
+  )
+}
 
 export const Default = Template.bind({})
