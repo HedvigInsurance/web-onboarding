473652fbc988a349578f0d43511c327a15bc973e
Ellen Styrélius <ellen@hedvig.com>
2022-01-31 11:42:32 +0100

    Add the Footer to CheckoutDetails and CheckoutPayment page components, with placeholder text for the button and 'buttonOnClick' functions that don't do anything :)

--- src/client/pages/CheckoutDetails/CheckoutDetails.tsx
+++ src/client/pages/CheckoutDetails/CheckoutDetails.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import { CheckoutPageWrapper } from 'components/checkout/CheckoutPageWrapper'
+import { Footer } from 'components/checkout/Footer'
+
+const BUTTON_TEXT_PLACEHOLDER = 'Continue to payment'
 
 export const CheckoutDetails = () => {
-  return <CheckoutPageWrapper>{}</CheckoutPageWrapper>
+  return (
+    <CheckoutPageWrapper>
+      <Footer
+        buttonText={BUTTON_TEXT_PLACEHOLDER}
+        buttonOnClick={() => {
+          console.log('click')
+        }}
+      />
+    </CheckoutPageWrapper>
+  )
 }
--- src/client/pages/CheckoutPayment/CheckoutPayment.tsx
+++ src/client/pages/CheckoutPayment/CheckoutPayment.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import { CheckoutPageWrapper } from 'components/checkout/CheckoutPageWrapper'
+import { Footer } from 'components/checkout/Footer'
+
+const BUTTON_TEXT_PLACEHOLDER = 'Complete purchase'
 
 export const CheckoutPayment = () => {
-  return <CheckoutPageWrapper>{}</CheckoutPageWrapper>
+  return (
+    <CheckoutPageWrapper>
+      <Footer
+        buttonText={BUTTON_TEXT_PLACEHOLDER}
+        buttonOnClick={() => {
+          console.log('click')
+        }}
+      />
+    </CheckoutPageWrapper>
+  )
 }
