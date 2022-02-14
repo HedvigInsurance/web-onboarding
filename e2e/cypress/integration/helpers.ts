844966aa44218bffcf6c75eb9e2c881ecb708c6f
Robin Andeer <robin@hedvig.com>
2022-02-14 16:33:19 +0100

    Add e2e test for NO checkout
    Add helper functions to create quote cart and quote bundle.

--- e2e/cypress/integration/helpers.ts
+++ e2e/cypress/integration/helpers.ts
@@ -6,3 +6,73 @@ export const getRandomBirthDateDK = () => {
   const month = String(pastDate.getMonth() + 1).padStart(2, '0')
   return `${date}-${month}-${pastDate.getFullYear()}`
 }
+
+const QUOTE_CART_MUTATION = /* GraphQL */ `
+  mutation CreateOnboardingQuoteCart($market: Market!, $locale: String!) {
+    onboardingQuoteCart_create(input: { market: $market, locale: $locale }) {
+      id
+    }
+  }
+`
+
+const QUOTE_BUNDLE_MUTATION = /* GraphQL */ `
+  mutation CreateQuoteBundle($id: ID!, $input: CreateQuoteBundleInput!) {
+    quoteCart_createQuoteBundle(id: $id, input: $input) {
+      ... on QuoteCart {
+        id
+        bundle {
+          quotes {
+            id
+          }
+        }
+      }
+      ... on QuoteBundleError {
+        message
+        type
+        limits {
+          code
+        }
+      }
+    }
+  }
+`
+
+const GRAPHQL_ENDPOINT = 'https://graphql.dev.hedvigit.com/graphql'
+
+type CreateQuoteBundleParams = {
+  quoteCartId: string
+  quotes: Array<any>
+}
+
+export const createQuoteBundle = ({
+  quoteCartId,
+  quotes,
+}: CreateQuoteBundleParams) => {
+  console.log(quotes)
+  return cy.request('POST', GRAPHQL_ENDPOINT, {
+    query: QUOTE_BUNDLE_MUTATION,
+    variables: {
+      id: quoteCartId,
+      input: {
+        payload: quotes,
+      },
+    },
+  })
+}
+
+type CreateQuoteCartParams = {
+  locale: string
+  market: string
+}
+
+export const createQuoteCart = ({ locale, market }: CreateQuoteCartParams) => {
+  return cy
+    .request('POST', GRAPHQL_ENDPOINT, {
+      query: QUOTE_CART_MUTATION,
+      variables: { locale, market },
+    })
+    .then((response) => {
+      const quoteCartId = response.body.data.onboardingQuoteCart_create.id
+      return quoteCartId
+    })
+}
