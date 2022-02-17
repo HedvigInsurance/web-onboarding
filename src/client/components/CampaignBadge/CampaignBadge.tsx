ab15bc08d7fcbf4bc603b83a684445332ddbbc1e
Robin Andeer <robin.andeer@gmail.com>
2022-02-17 09:11:22 +0100

    Simplify CSS
    Co-authored-by: Gustav Eén <gustaveen@gmail.com>

--- src/client/components/CampaignBadge/CampaignBadge.tsx
+++ src/client/components/CampaignBadge/CampaignBadge.tsx
@@ -12,7 +12,8 @@ const InlineFlexGap = styled.div`
   --gap: 0.5rem;
   display: inline-flex;
   flex-wrap: wrap;
-  margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
+  margin-top: calc(-1 * var(--gap));
+  margin-left: calc(-1 * var(--gap));
   width: calc(100% + var(--gap));
 
   > * {
