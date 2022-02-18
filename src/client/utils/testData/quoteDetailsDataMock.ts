df3185dc3e0899d16907e458abf106aeb50eeab3
Ellen Styrélius <ellen@hedvig.com>
2022-02-18 17:48:42 +0100

    Fix existing and add some more mocked data in quoteDetailsDataMock.ts

--- src/client/utils/testData/quoteDetailsDataMock.ts
+++ src/client/utils/testData/quoteDetailsDataMock.ts
@@ -1,22 +1,56 @@
-import { TypeOfContract } from 'data/graphql'
-import { QuoteDetailsData } from '../hooks/useQuoteDetailsData'
+import { TypeOfContract, ExtraBuildingType } from 'data/graphql'
+import { QuoteDetailsData } from 'api/quoteDetailsDataTypes'
 
-export const quoteDetailsDataMockRentalSE = {
-  SE_APARTMENT_RENT: {
-    id: 'af271e4d-f70c-4e05-ac71-c8b0fd9dee4f',
-    ssn: '199006081234',
-    birthDate: '1990-06-08',
-    firstName: 'Svea',
-    lastName: 'Svensson',
-    email: 'svea@svensson.se',
-    phoneNumber: null,
-    street: 'Svenssonvägen 65',
-    city: null,
-    zipCode: '12649',
+export const quoteDetailsDataMockRentalSE: QuoteDetailsData = [
+  {
+    id: 'QUOTE_ID_SE_APARTMENT_RENT',
+    typeOfContract: TypeOfContract.SeApartmentRent,
+    street: 'Hyresvägen 1',
+    zipCode: '12345',
     numberCoInsured: 1,
     livingSpace: 45,
-    subType: 'RENT',
-    typeOfContract: TypeOfContract.SeApartmentRent,
     isStudent: false,
   },
-}
+]
+
+export const quoteDetailsDataMockBrfAccidentSE: QuoteDetailsData = [
+  {
+    id: 'QUOTE_ID_SE_APARTMENT_BRF',
+    typeOfContract: TypeOfContract.SeApartmentBrf,
+    street: 'Bostadsrättsvägen 1',
+    zipCode: '12345',
+    livingSpace: 45,
+    numberCoInsured: 1,
+    isStudent: false,
+  },
+  {
+    id: 'QUOTE_ID_SE_ACCIDENT',
+    typeOfContract: TypeOfContract.SeAccident,
+    numberCoInsured: 1,
+    isStudent: false,
+  },
+]
+
+export const quoteDetailsDataMockHouseSE: QuoteDetailsData = [
+  {
+    id: 'QUOTE_ID_SE_HOUSE',
+    typeOfContract: TypeOfContract.SeHouse,
+    street: 'Husvägen 1',
+    zipCode: '12649',
+    livingSpace: 145,
+    numberCoInsured: 2,
+    isStudent: false,
+    numberOfBathrooms: 2,
+    yearOfConstruction: 1977,
+    ancillaryArea: 60,
+    isSubleted: false,
+    extraBuildings: [
+      {
+        type: ExtraBuildingType.Garage,
+        displayName: 'Garage',
+        area: 10,
+        hasWaterConnected: false,
+      },
+    ],
+  },
+]
