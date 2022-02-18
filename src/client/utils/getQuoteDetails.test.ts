6dec8c0b3ec178220fa01dbc38a689b185aa9a29
Ellen Styrélius <ellen@hedvig.com>
2022-02-18 17:53:13 +0100

    Fix test for 'getQuoteDetails' function with Swedish House quote data

--- src/client/utils/getQuoteDetails.test.ts
+++ src/client/utils/getQuoteDetails.test.ts
@@ -1,12 +1,28 @@
-import { quoteDetailsDataMockRentalSE } from 'utils/testData/quoteDetailsDataMock'
-import { renderHook } from '../test/utils'
+import { quoteDetailsDataMockHouseSE } from 'utils/testData/quoteDetailsDataMock'
+import { typeOfResidenceTextKeys } from 'pages/OfferNew/utils'
 import { getQuoteDetails } from './getQuoteDetails'
-import { useTextKeys } from './textKeys'
 
 describe('getQuoteDetails function', () => {
-  it('returns an array with expected data - Swedish Apartment', () => {
-    const textKeys = renderHook(() => useTextKeys())
+  describe('with Swedish House quote', () => {
+    const swedishHouseQuoteDetails = getQuoteDetails({
+      quoteDetailsData: quoteDetailsDataMockHouseSE,
+    })
+    it('returns the correct amount of data', () => {
+      expect(swedishHouseQuoteDetails.length).toBe(3)
+      expect(swedishHouseQuoteDetails[0].length).toBe(8)
+      expect(swedishHouseQuoteDetails[1].length).toBe(3)
+      expect(swedishHouseQuoteDetails[2].length).toBe(1)
+    })
+    it('returns textKey strings instead of raw values for typeOfResidence and isSubleted', () => {
+      const typeOfResidenceData = swedishHouseQuoteDetails[0][3]
+      const isSubleted = swedishHouseQuoteDetails[0][7]
 
-    //const result = getQuoteDetails(quoteDetailsDataMockRentalSE, textKeys)
+      expect(typeOfResidenceData.value.value).toBe(undefined)
+      expect(typeOfResidenceData.value.textKey).toBe(
+        typeOfResidenceTextKeys.SE_HOUSE,
+      )
+      expect(isSubleted.value.value).toBe(undefined)
+      expect(isSubleted.value.textKey).toBe('CHECKOUT_DETAILS_IS_NOT_SUBLETED')
+    })
   })
 })
