d42695882afad78cac39d0404d0724ca8073340e
Ellen Styrélius <ellen@hedvig.com>
2022-03-03 18:15:05 +0100

    Several changes in CheckoutPageErrorModal: add a text button to open Intercom, make OutlinedButton a LinkButton instead of regular button and make it link to landing page, fix some styling

--- src/client/pages/Checkout/shared/CheckoutPageErrorModal.tsx
+++ src/client/pages/Checkout/shared/CheckoutPageErrorModal.tsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
+import { useCurrentLocale } from 'l10n/useCurrentLocale'
 import { useTextKeys } from 'utils/textKeys'
+import { openIntercomChat } from 'utils/intercom.helpers'
 import { ErrorModal } from 'components/ErrorModal'
 import { Headline } from 'components/Headline/Headline'
-import { Button } from 'components/buttons'
+import { Button, TextButton, LinkButton } from 'components/buttons'
 import { LoadingDots } from 'components/LoadingDots/LoadingDots'
+import { landingRoute } from '../../../../routes'
 
 const { gray700, gray900 } = colorsV3
+const bodyFontSize = '1rem'
 
 const ContentWrapper = styled.div`
   display: grid;
@@ -19,7 +23,7 @@ const Text = styled.div`
   color: ${gray700};
   font-style: normal;
   font-weight: normal;
-  font-size: 1rem;
+  font-size: ${bodyFontSize};
   line-height: 1.5rem;
 `
 const ButtonsContainer = styled.div`
@@ -27,10 +31,9 @@ const ButtonsContainer = styled.div`
   flex-wrap: wrap;
   gap: 0.5rem;
 `
-const OutlinedButton = styled(Button)`
+const OutlinedButton = styled(LinkButton)`
   background-color: transparent;
   border: 1px solid ${gray900};
-  color: ${gray900};
 `
 const LoadingDotsContainer = styled.div`
   width: 100%;
@@ -40,16 +43,22 @@ const LoadingDotsContainer = styled.div`
   align-items: center;
   position: absolute;
   bottom: 1rem;
+  left: 0;
+`
+const InlineTextButton = styled(TextButton)`
+  display: inline;
+  font-size: ${bodyFontSize};
 `
 
 export const CheckoutPageErrorModal = () => {
   const [isReloading, setIsReloading] = useState(false)
   const textKeys = useTextKeys()
   const { go } = useHistory()
+  const { path: localePath } = useCurrentLocale()
 
   const reloadPage = () => {
     setIsReloading(true)
-    go(0) // Reload current page
+    go(0)
   }
 
   return (
@@ -58,12 +67,21 @@ export const CheckoutPageErrorModal = () => {
         <Headline variant="xs" colorVariant="dark" headingLevel="h3">
           {textKeys.CHECKOUT_PAGE_ERROR_MODAL_HEADING()}
         </Headline>
-        <Text>{textKeys.CHECKOUT_PAGE_ERROR_MODAL_BODY()}</Text>
+        <Text>
+          {textKeys.CHECKOUT_PAGE_ERROR_MODAL_BODY()}{' '}
+          <InlineTextButton onClick={openIntercomChat}>
+            {textKeys.CHECKOUT_PAGE_ERROR_MODAL_OPEN_CHAT()}.
+          </InlineTextButton>
+        </Text>
         <ButtonsContainer>
           <Button background={gray900} onClick={reloadPage}>
-            {textKeys.CHECKOUT_PAGE_ERROR_MODAL_RETRY_BUTTON()}
+            {textKeys.GENERIC_ERROR_ACTION_RETRY()}
           </Button>
-          <OutlinedButton background={gray900}>
+          <OutlinedButton
+            to={`/${localePath}${landingRoute}`}
+            background={gray900}
+            foreground={gray900}
+          >
             {textKeys.CHECKOUT_PAGE_ERROR_MODAL_BACK_TO_START_BUTTON()}
           </OutlinedButton>
         </ButtonsContainer>
