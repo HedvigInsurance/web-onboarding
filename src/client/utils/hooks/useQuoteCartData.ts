0cf882ced1a4e55d5f3de29f5b03f85d293e18ca
Dan P <dan.popoutanu@hedvig.com>
2022-03-17 13:39:49 +0200

    get functions out of hook

--- src/client/utils/hooks/useQuoteCartData.ts
+++ src/client/utils/hooks/useQuoteCartData.ts
@@ -13,8 +13,6 @@ import {
   HomeInsuranceTypeOfContract,
 } from 'pages/OfferNew/utils'
 import { GenericQuoteData } from '/pages/OfferNew/types'
-import { HouseQuoteDetails } from 'src/client/api/quoteDetailsDataTypes'
-import { isHouseQuoteDetailsData } from 'api/quoteDetailsDataSelectors'
 import { formatPostalNumber } from '../postalNumbers'
 import { useSelectedInsuranceTypes } from './useSelectedInsuranceTypes'
 
@@ -31,6 +29,142 @@ type Details = {
 }
 
 type DetailsGroup = Details[]
+
+const getHomeDetails = (quoteDetails: GenericQuoteData) => {
+  const { typeOfContract } = quoteDetails
+
+  const { street } = quoteDetails
+
+  if ('floor' in quoteDetails && 'apartment' in quoteDetails) {
+    const { floor, apartment } = quoteDetails
+
+    if (floor || apartment) {
+      const formattedFloor = floor ? `${floor}.` : ''
+      const apartmentString = apartment ? ` ${apartment}` : ''
+      street === `${street}, ${formattedFloor}${apartmentString}`
+    }
+  }
+  return [
+    {
+      label: 'CHECKOUT_DETAILS_ADDRESS',
+      value: { value: street },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_ZIPCODE',
+      value: {
+        value: quoteDetails.zipCode && formatPostalNumber(quoteDetails.zipCode),
+      },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_LIVING_SPACE',
+      value: {
+        value: quoteDetails.livingSpace,
+        suffix: 'CHECKOUT_DETAILS_SQM_SUFFIX',
+      },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_RESIDENCE_TYPE',
+      value: {
+        textKey:
+          typeOfResidenceTextKeys[
+            typeOfContract as HomeInsuranceTypeOfContract
+          ],
+      },
+    },
+
+    ...getHouseDetails(quoteDetails),
+  ]
+}
+
+const getHouseDetails = (data: GenericQuoteData) => {
+  if (!data) {
+    return []
+  }
+  return [
+    {
+      label: 'CHECKOUT_DETAILS_ANCILLARY_SPACE',
+      value: {
+        value: data.ancillaryArea,
+        suffix: 'CHECKOUT_DETAILS_SQM_SUFFIX',
+      },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS',
+      value: {
+        value: data.numberOfBathrooms,
+        suffix:
+          data.numberOfBathrooms && data.numberOfBathrooms > 1
+            ? 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS_SUFFIX_MANY'
+            : 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS_SUFFIX_ONE',
+      },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_YEAR_OF_CONSTRUCTION',
+      value: { value: data.yearOfConstruction },
+    },
+    {
+      label: 'CHECKOUT_DETAILS_IS_PARTLY_SUBLET',
+      value: {
+        textKey: data.isSubleted ? 'YES' : 'NO',
+      },
+    },
+  ]
+}
+
+const getExtraBuildingsDetails = (quoteDetails: GenericQuoteData) => {
+  if (!quoteDetails.extraBuildings) {
+    return []
+  }
+  const extraBuildings = quoteDetails.extraBuildings.map(
+    (extraBuilding: any) => [
+      {
+        label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_BUILDING_TYPE',
+        value: { value: extraBuilding.displayName },
+      },
+      {
+        label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_SIZE',
+        value: {
+          value: extraBuilding.area,
+          suffix: 'CHECKOUT_DETAILS_SQM_VALUE',
+        },
+      },
+      {
+        label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_HAS_WATER_CONNECTED',
+        value: { textKey: extraBuilding.hasWaterConnected ? 'YES' : 'NO' },
+      },
+    ],
+  )
+  return extraBuildings
+}
+
+const getPeopleDetails = (quoteDetails: GenericQuoteData) => {
+  const { numberCoInsured } = quoteDetails
+  const householdSize = numberCoInsured + 1
+  return [
+    {
+      label: 'CHECKOUT_DETAILS_HOUSEHOLD_SIZE',
+      value: {
+        value: householdSize,
+        suffix:
+          householdSize > 1
+            ? 'CHECKOUT_DETAILS_NUMBER_OF_PEOPLE_SUFFIX_MANY'
+            : 'CHECKOUT_DETAILS_NUMBER_OF_PEOPLE_SUFFIX_ONE',
+      },
+    },
+    ...getStudentOrYouth(quoteDetails),
+  ]
+}
+
+const getStudentOrYouth = (data: GenericQuoteData) => {
+  if (data.isStudent) {
+    return [{ label: '+', value: { textKey: 'CHECKOUT_DETAILS_STUDENT' } }]
+  }
+  if (data.isYouth) {
+    return [{ label: '+', value: { textKey: 'CHECKOUT_DETAILS_YOUTH' } }]
+  }
+
+  return []
+}
 
 export const useQuoteCartData = () => {
   const { id: quoteCartId } = useParams<{ id: string }>()
@@ -61,144 +195,10 @@ export const useQuoteCartData = () => {
   }
   const quoteDetails = mainQuote.data
 
-  const getHomeDetails = (): DetailsGroup => {
-    const { typeOfContract } = quoteDetails
-
-    const { street } = quoteDetails
-
-    if ('floor' in quoteDetails && 'apartment' in quoteDetails) {
-      const { floor, apartment } = quoteDetails
-
-      if (floor || apartment) {
-        const formattedFloor = floor ? `${floor}.` : ''
-        const apartmentString = apartment ? ` ${apartment}` : ''
-        street === `${street}, ${formattedFloor}${apartmentString}`
-      }
-    }
-    return [
-      {
-        label: 'CHECKOUT_DETAILS_ADDRESS',
-        value: { value: street },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_ZIPCODE',
-        value: { value: formatPostalNumber(quoteDetails.zipCode) },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_LIVING_SPACE',
-        value: {
-          value: quoteDetails.livingSpace,
-          suffix: 'CHECKOUT_DETAILS_SQM_SUFFIX',
-        },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_RESIDENCE_TYPE',
-        value: {
-          textKey:
-            typeOfResidenceTextKeys[
-              typeOfContract as HomeInsuranceTypeOfContract
-            ],
-        },
-      },
-
-      ...getHouseDetails(quoteDetails),
-    ]
-  }
-
-  const getHouseDetails = (data: HouseQuoteDetails) => {
-    if (!isHouseQuoteDetailsData(data)) {
-      return []
-    }
-    return [
-      {
-        label: 'CHECKOUT_DETAILS_ANCILLARY_SPACE',
-        value: {
-          value: data.ancillaryArea,
-          suffix: 'CHECKOUT_DETAILS_SQM_SUFFIX',
-        },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS',
-        value: {
-          value: data.numberOfBathrooms,
-          suffix:
-            data.numberOfBathrooms > 1
-              ? 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS_SUFFIX_MANY'
-              : 'CHECKOUT_DETAILS_NUMBER_OF_BATHROOMS_SUFFIX_ONE',
-        },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_YEAR_OF_CONSTRUCTION',
-        value: { value: data.yearOfConstruction },
-      },
-      {
-        label: 'CHECKOUT_DETAILS_IS_PARTLY_SUBLET',
-        value: {
-          textKey: data.isSubleted ? 'YES' : 'NO',
-        },
-      },
-    ]
-  }
-
-  const getExtraBuildingsDetails = (): DetailsGroup[] => {
-    if (!quoteDetails.extraBuilding) {
-      return []
-    }
-    const extraBuildings = quoteDetails.extraBuildings.map(
-      (extraBuilding: any) => [
-        {
-          label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_BUILDING_TYPE',
-          value: { value: extraBuilding.displayName },
-        },
-        {
-          label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_SIZE',
-          value: {
-            value: extraBuilding.area,
-            suffix: 'CHECKOUT_DETAILS_SQM_VALUE',
-          },
-        },
-        {
-          label: 'CHECKOUT_DETAILS_EXTRA_BUILDINGS_HAS_WATER_CONNECTED',
-          value: { textKey: extraBuilding.hasWaterConnected ? 'YES' : 'NO' },
-        },
-      ],
-    )
-    return extraBuildings
-  }
-
-  const getPeopleDetails = (): DetailsGroup => {
-    const { numberCoInsured } = quoteDetails
-    const householdSize = numberCoInsured + 1
-    return [
-      {
-        label: 'CHECKOUT_DETAILS_HOUSEHOLD_SIZE',
-        value: {
-          value: householdSize,
-          suffix:
-            householdSize > 1
-              ? 'CHECKOUT_DETAILS_NUMBER_OF_PEOPLE_SUFFIX_MANY'
-              : 'CHECKOUT_DETAILS_NUMBER_OF_PEOPLE_SUFFIX_ONE',
-        },
-      },
-      ...getStudentOrYouth(quoteDetails),
-    ]
-  }
-
-  const getStudentOrYouth = (data: GenericQuoteData) => {
-    if (data.isStudent) {
-      return [{ label: '+', value: { textKey: 'CHECKOUT_DETAILS_STUDENT' } }]
-    }
-    if (data.isYouth) {
-      return [{ label: '+', value: { textKey: 'CHECKOUT_DETAILS_YOUTH' } }]
-    }
-
-    return []
-  }
-
   const quoteDetailsGroups: DetailsGroup[] = [
-    getHomeDetails(),
-    ...getExtraBuildingsDetails(),
-    getPeopleDetails(),
+    getHomeDetails(quoteDetails),
+    ...getExtraBuildingsDetails(quoteDetails),
+    getPeopleDetails(quoteDetails),
   ]
 
   const userDetails = {
