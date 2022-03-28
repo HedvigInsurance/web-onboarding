c48f753e5d259d416eba03cac40ed1a5bbeeb57d
Simon Aunér <simonauner@gmail.com>
2022-03-28 08:07:19 +0200

    Remove React.FC

--- src/client/components/ModalNew.tsx
+++ src/client/components/ModalNew.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { useLockBodyScroll } from '../utils/hooks/useLockBodyScroll'
 import { CloseButton } from './CloseButton/CloseButton'
 
@@ -10,6 +10,7 @@ export type ModalProps = {
   dynamicHeight?: boolean
   onClose?: () => void
   className?: string
+  children?: ReactNode
 }
 
 const Wrapper = styled(motion.div)`
@@ -79,13 +80,13 @@ const ModalInnerContainer = styled.div`
   height: 100%;
 `
 
-export const Modal: React.FC<ModalProps> = ({
+export const Modal = ({
   isVisible,
   dynamicHeight,
   className,
   onClose,
   children,
-}) => {
+}: ModalProps) => {
   useLockBodyScroll({ isLocked: isVisible })
   const hasOnCloseFunction = Boolean(onClose)
 
@@ -158,6 +159,11 @@ export const Modal: React.FC<ModalProps> = ({
   )
 }
 
+type ModalFooterProps = {
+  sticky?: boolean
+  children?: ReactNode
+}
+
 const Footer = styled.div<ModalFooterProps>`
   padding: 1rem 0;
   display: flex;
@@ -173,10 +179,7 @@ const Footer = styled.div<ModalFooterProps>`
     bottom: 0;
 `}
 `
-type ModalFooterProps = {
-  sticky?: boolean
-}
 
-export const ModalFooter: React.FC<ModalFooterProps> = ({ children }) => {
+export const ModalFooter = ({ children }: ModalFooterProps) => {
   return <Footer>{children}</Footer>
 }
