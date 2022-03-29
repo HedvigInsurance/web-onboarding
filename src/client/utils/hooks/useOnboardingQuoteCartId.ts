09a68ea7b9eed9e9e7fe35245e4d172c31d3b152
Robin Andeer <robin.andeer@gmail.com>
2022-03-29 17:48:02 +0200

    Move constant outside hook

--- src/client/utils/hooks/useOnboardingQuoteCartId.ts
+++ src/client/utils/hooks/useOnboardingQuoteCartId.ts
@@ -1,8 +1,9 @@
 import { CookieStorage } from 'cookie-storage'
 import { ONBOARDING_QUOTE_CART_COOKIE_KEY } from 'shared/sessionStorage'
 
+const cookieStorage = new CookieStorage()
+
 export const useOnboardingQuoteCartId = () => {
-  const cookieStorage = new CookieStorage()
   const quoteCartId = cookieStorage.getItem(ONBOARDING_QUOTE_CART_COOKIE_KEY)
 
   if (quoteCartId === null) throw new Error('No quote cart id found')
