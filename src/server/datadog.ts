ddf4fa4ba6961fb0ea5feb9acf9224ce3a3cc53e
Robin Andeer <robin.andeer@gmail.com>
2022-04-01 17:15:37 +0200

    Update tracer setup

--- src/server/datadog.ts
+++ src/server/datadog.ts
@@ -4,6 +4,10 @@ import { APP_ENVIRONMENT, HEROKU_SLUG_COMMIT } from './config'
 tracer.init({
   env: APP_ENVIRONMENT,
   version: HEROKU_SLUG_COMMIT,
+  service: 'web-onboarding',
+  plugins: false,
 })
+
+tracer.use('koa')
 
 export default tracer
