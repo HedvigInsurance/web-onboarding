7dab8ba356343f8c887bb3fff1eff298b5762b8c
Dan P <dan.popoutanu@hedvig.com>
2022-04-11 10:50:29 +0300

    reorder

--- src/server/adyenMiddleware.ts
+++ src/server/adyenMiddleware.ts
@@ -24,6 +24,71 @@ export const handleAdyen3dsPostRedirect: Router.IMiddleware<
 > = async (ctx, _next) => {
   const { MD: md, PaRes: pares } = (ctx.request as any).body as Adyen3dsDetails
   const session = createSession<Session>(new ServerCookieStorage(ctx))
+  const Authorization = session.getSession()?.token
+  try {
+    const result = await httpClient.post(
+      GIRAFFE_HOST + '/graphql',
+      {
+        operationName: 'SubmitAdyenRedirection',
+        query: `
+        mutation SubmitAdyenRedirection($md: String!, $pares: String!) {
+          submitAdyenRedirection(req: { md: $md, pares: $pares }) {
+            resultCode
+          }
+        }
+      `,
+        variables: {
+          md,
+          pares,
+        },
+      },
+      {
+        headers: { Authorization },
+      },
+    )
+
+    if (result.status !== 200) {
+      throw new Error(
+        `Expected status code from Graphql endpoint to be 200 when submitting adyen redirection, but was really ${result.status}`,
+      )
+    }
+
+    if (
+      ['Authorised', 'Pending'].includes(
+        result.data?.data?.submitAdyenRedirection?.resultCode,
+      )
+    ) {
+      ctx.redirect(
+        ctx.params.locale
+          ? `/${ctx.params.locale}/new-member/download`
+          : '/new-member/connect-download',
+      )
+      ctx.body = 'Loading'
+      return
+    }
+
+    const message = `Received error adyen resultCode "${result.data?.data?.submitAdyenRedirection?.resultCode}" when submitting adyen redirection`
+    Sentry.captureException(new Error(message))
+    ctx.state.getLogger('ayden').error(message)
+    ctx.redirect(
+      ctx.params.locale
+        ? `/${ctx.params.locale}/new-member/connect-payment?error=yes` // todo handle client side
+        : '/new-member/connect-payment?error=yes',
+    )
+    ctx.body = 'Loading'
+  } catch (e) {
+    Sentry.captureException(e)
+    ctx.state.getLogger('ayden').error(e.message)
+    throw e
+  }
+}
+
+export const handleNewAdyen3dsPostRedirect: Router.IMiddleware<
+  WithLoggerState,
+  any
+> = async (ctx, _next) => {
+  const { MD: md, PaRes: pares } = (ctx.request as any).body as Adyen3dsDetails
+  const session = createSession<Session>(new ServerCookieStorage(ctx))
   const paymentTokenId = session.getSession()?.paymentTokenId
 
   try {
@@ -53,72 +118,6 @@ export const handleAdyen3dsPostRedirect: Router.IMiddleware<
     if (
       ['Authorised', 'Pending'].includes(
         result.data?.data?.paymentConnection_submitAdyenRedirection?.status,
-      )
-    ) {
-      ctx.redirect(
-        ctx.params.locale
-          ? `/${ctx.params.locale}/new-member/download`
-          : '/new-member/connect-download',
-      )
-      ctx.body = 'Loading'
-      return
-    }
-
-    const message = `Received error adyen resultCode "${result.data?.data?.submitAdyenRedirection?.resultCode}" when submitting adyen redirection`
-    Sentry.captureException(new Error(message))
-    ctx.state.getLogger('ayden').error(message)
-    ctx.redirect(
-      ctx.params.locale
-        ? `/${ctx.params.locale}/new-member/connect-payment?error=yes` // todo handle client side
-        : '/new-member/connect-payment?error=yes',
-    )
-    ctx.body = 'Loading'
-  } catch (e) {
-    // console.log(e)
-    Sentry.captureException(e)
-    ctx.state.getLogger('ayden').error(e.message)
-    throw e
-  }
-}
-
-export const handleNewAdyen3dsPostRedirect: Router.IMiddleware<
-  WithLoggerState,
-  any
-> = async (ctx, _next) => {
-  const { MD: md, PaRes: pares } = (ctx.request as any).body as Adyen3dsDetails
-  const session = createSession<Session>(new ServerCookieStorage(ctx))
-  const Authorization = session.getSession()?.token
-  try {
-    const result = await httpClient.post(
-      GIRAFFE_HOST + '/graphql',
-      {
-        operationName: 'SubmitAdyenRedirection',
-        query: `
-          mutation SubmitAdyenRedirection($md: String!, $pares: String!) {
-            submitAdyenRedirection(req: { md: $md, pares: $pares }) {
-              resultCode
-            }
-          }
-        `,
-        variables: {
-          md,
-          pares,
-        },
-      },
-      {
-        headers: { Authorization },
-      },
-    )
-
-    if (result.status !== 200) {
-      throw new Error(
-        `Expected status code from Graphql endpoint to be 200 when submitting adyen redirection, but was really ${result.status}`,
-      )
-    }
-
-    if (
-      ['Authorised', 'Pending'].includes(
-        result.data?.data?.submitAdyenRedirection?.resultCode,
       )
     ) {
       ctx.redirect(
