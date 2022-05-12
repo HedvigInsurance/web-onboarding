cfa479adb5408c16c634f10c52c91d2e89b9d215
Robin Andeer <robin.andeer@gmail.com>
2022-05-12 12:18:40 +0200

    Remove auth header from API calls
    We currently send "undefined" as a header which makes the API confused.

--- src/client/pages/Checkout/CheckoutPayment/index.tsx
+++ src/client/pages/Checkout/CheckoutPayment/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useQuoteCartData } from 'utils/hooks/useQuoteCartData'
 import { LoadingPage } from 'components/LoadingPage'
 import { CheckoutErrorModal, onRetry } from '../shared/ErrorModal'
+import { apolloClient as realApolloClient } from '../../../apolloClient'
 import { CheckoutPayment } from './CheckoutPayment'
 
 export const Checkout = () => {
   const data = useQuoteCartData()
+
+  useEffect(() => {
+    // clean up existing auth tokens
+    if (realApolloClient) {
+      delete realApolloClient.httpLink.options.headers.authorization
+    }
+  }, [])
 
   if (data?.error) {
     console.error('Quote cart data error: no data')
