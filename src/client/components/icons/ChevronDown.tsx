ede368c10889f85e88d7dff5470a01fd6c49667a
Amanda Flodström <amanda@hedvig.com>
2022-05-30 14:06:07 +0200

    Fix

--- src/client/components/icons/ChevronDown.tsx
+++ src/client/components/icons/ChevronDown.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { colorsV3 } from '@hedviginsurance/brand'
 import { IconRoot, IconRootProps } from './IconRoot'
 
-export const ChevronDown: React.FC<IconRootProps> = (props) => (
+export const ChevronDown = (props: IconRootProps) => (
   <IconRoot {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path
       fillRule="evenodd"
--- src/client/components/icons/ChevronUp.tsx
+++ src/client/components/icons/ChevronUp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { colorsV3 } from '@hedviginsurance/brand'
 import { IconRoot, IconRootProps } from './IconRoot'
 
-export const ChevronUp: React.FC<IconRootProps> = (props) => (
+export const ChevronUp = (props: IconRootProps) => (
   <IconRoot {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path
       fillRule="evenodd"
