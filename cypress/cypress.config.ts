c4eb7bb1393febc1cf4d55afd8f44de118edb2f0
Guilherme Popolin <guilherme@hedvig.com>
2022-07-06 18:03:47 +0200

    refact(e2e): migrate tests

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     supportFile: false,
     fixturesFolder: false,
+    specPattern: 'cypress/e2e/**/*.spec.{ts,tsx}',
     baseUrl: 'https://www.dev.hedvigit.com',
     experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 10000,
