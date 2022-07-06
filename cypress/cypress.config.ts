7cc1c031853d0f25155a079d0d9d9beb2c3198a1
Guilherme Popolin <guilherme@hedvig.com>
2022-07-06 14:42:08 +0200

    reafact(e2e): migrate cypress config

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {},
+  e2e: {
+    supportFile: false,
+    fixturesFolder: false,
+    baseUrl: 'https://www.dev.hedvigit.com',
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+  },
 })
