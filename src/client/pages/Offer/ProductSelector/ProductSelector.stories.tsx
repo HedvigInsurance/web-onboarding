3bb9437310f1e7706877bd9ec39733b2065b013f
Guilherme Popolin <guilherme@hedvig.com>
2022-08-12 10:42:14 +0200

    fix(ProductSelector): added text keys support for storybook stories

--- src/client/pages/Offer/ProductSelector/ProductSelector.stories.tsx
+++ src/client/pages/Offer/ProductSelector/ProductSelector.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-
+import { TextKeyProvider } from 'utils/textKeys'
 import { ProductSelector } from './ProductSlector'
 
 export default {
@@ -13,39 +13,41 @@ export default {
 
 export const Default: ComponentStory<typeof ProductSelector> = () => {
   return (
-    <ProductSelector
-      mainProducts={[
-        {
-          id: 'home-insurance',
-          name: 'Home Insurance',
-          description: 'Coverage for your house or apartment contents',
-          price: '218 NOK/mth',
-          image: 'https://via.placeholder.com/400x300.png',
-        },
-        {
-          id: 'house-insurance',
-          name: 'House Insurance',
-          description: 'Coverage for your house and buildings',
-          price: '400 NOK/mth',
-          image: 'https://via.placeholder.com/400x300.png',
-        },
-      ]}
-      additionalProducts={[
-        {
-          id: 'accident-insurance',
-          name: 'Accident Insurance',
-          description: 'Extra coverage in case of an accident',
-          price: '39 NOK/mth',
-          image: 'https://via.placeholder.com/400x300.png',
-        },
-        {
-          id: 'travel-insurance',
-          name: 'Travel Insurance',
-          description: 'Covers you and your family when you are traveling',
-          price: '39 NOK/mth',
-          image: 'https://via.placeholder.com/400x300.png',
-        },
-      ]}
-    />
+    <TextKeyProvider locale="en_NO">
+      <ProductSelector
+        mainProducts={[
+          {
+            id: 'home-insurance',
+            name: 'Home Insurance',
+            description: 'Coverage for your house or apartment contents',
+            price: '218 NOK/mth',
+            image: 'https://via.placeholder.com/400x300.png',
+          },
+          {
+            id: 'house-insurance',
+            name: 'House Insurance',
+            description: 'Coverage for your house and buildings',
+            price: '400 NOK/mth',
+            image: 'https://via.placeholder.com/400x300.png',
+          },
+        ]}
+        additionalProducts={[
+          {
+            id: 'accident-insurance',
+            name: 'Accident Insurance',
+            description: 'Extra coverage in case of an accident',
+            price: '39 NOK/mth',
+            image: 'https://via.placeholder.com/400x300.png',
+          },
+          {
+            id: 'travel-insurance',
+            name: 'Travel Insurance',
+            description: 'Covers you and your family when you are traveling',
+            price: '39 NOK/mth',
+            image: 'https://via.placeholder.com/400x300.png',
+          },
+        ]}
+      />
+    </TextKeyProvider>
   )
 }
