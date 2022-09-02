5701f0913b320d73adf482e24cd81daa65c034e6
Simon Aunér <simonauner@gmail.com>
2022-09-02 11:41:53 +0200

    Introduce size prop for Notice
    This is because the font sizes and paddings are larger on the Download page compared to the Offer page

--- src/client/components/Notice/Notice.tsx
+++ src/client/components/Notice/Notice.tsx
@@ -1,22 +1,35 @@
 import styled from '@emotion/styled'
 import { colorsV3 } from '@hedviginsurance/brand'
 import React from 'react'
+import { MEDIA_QUERIES } from 'utils/mediaQueries'
 
-export const RootElement = styled.div(() => ({
-  display: 'flex',
-  backgroundColor: colorsV3.purple100,
-  padding: '0.75rem 1rem ',
-  borderRadius: '0.5rem',
-}))
+export const RootElement = styled.div(
+  ({ size }: Pick<NoticeProps, 'size'>) => ({
+    display: 'flex',
+    backgroundColor: colorsV3.purple100,
+    padding: size === 'sm' ? '0.75rem 1rem' : '1rem',
+    borderRadius: '0.5rem',
+    [MEDIA_QUERIES.mediumScreen]: {
+      padding: size === 'sm' ? '0.75rem 1rem ' : '1.5rem',
+    },
+  }),
+)
 
-export const Header = styled.p({
-  fontSize: '0.875rem',
+export const Header = styled.p(({ size }: Pick<NoticeProps, 'size'>) => ({
+  fontSize: size === 'sm' ? '0.875rem' : '1rem',
   color: colorsV3.black,
   display: 'flex',
   alignItems: 'center',
   margin: 0,
   marginBottom: '0.5rem',
-})
+  // Override line-height of the content-wrapper, otherwise heading
+  // will not be aligned when size is md
+  lineHeight: size === 'sm' ? '1rem' : '1.25rem',
+  [MEDIA_QUERIES.mediumScreen]: {
+    fontSize: size === 'sm' ? '0.875rem' : '1.25rem',
+    lineHeight: size === 'sm' ? '1rem' : '1.5rem',
+  },
+}))
 
 const IconWrapper = styled.div({
   marginRight: '0.5rem',
@@ -24,13 +37,17 @@ const IconWrapper = styled.div({
   lineHeight: 0,
 })
 
-const ContentWrapper = styled.div({
-  fontSize: '0.75rem',
-  lineHeight: '1rem',
+const ContentWrapper = styled.div(({ size }: Pick<NoticeProps, 'size'>) => ({
+  fontSize: size === 'sm' ? '0.75rem' : '0.875rem',
+  lineHeight: size === 'sm' ? '1rem' : '1.25rem',
   color: colorsV3.gray700,
   // Aligns the content with the icon
-  paddingTop: '1px',
-})
+  marginTop: size === 'sm' ? '1px' : '-2px',
+  [MEDIA_QUERIES.mediumScreen]: {
+    fontSize: size === 'sm' ? '0.75rem' : '1rem',
+    lineHeight: size === 'sm' ? '1rem' : '1.5rem',
+  },
+}))
 
 export type NoticeProps = {
   /**
@@ -39,13 +56,30 @@ export type NoticeProps = {
   icon?: React.ReactElement
   children: React.ReactNode
   className?: string
+  size?: 'sm' | 'md'
 }
 
-export const Root = ({ icon, children, className }: NoticeProps) => {
+export const Root = ({
+  icon,
+  children,
+  className,
+  size = 'md',
+}: NoticeProps) => {
+  // Passes `size` to any child components (so that any eventual Notice.Header also gets the prop)
+  const clonedChildren = React.Children.map(children, (child) => {
+    if (!React.isValidElement(child)) {
+      return child
+    }
+
+    return React.cloneElement(child, {
+      size,
+    })
+  })
+
   return (
-    <RootElement className={className}>
+    <RootElement className={className} size={size}>
       {icon ? <IconWrapper>{icon}</IconWrapper> : null}
-      <ContentWrapper>{children}</ContentWrapper>
+      <ContentWrapper size={size}>{clonedChildren}</ContentWrapper>
     </RootElement>
   )
 }
