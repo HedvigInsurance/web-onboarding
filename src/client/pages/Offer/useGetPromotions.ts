240d21c112cbc4a9a6e1583170697a46be74fbd0
Simon Aunér <simonauner@gmail.com>
2022-10-03 15:29:18 +0200

    Only show Car Switcher promo if member is a car switcher

--- src/client/pages/Offer/useGetPromotions.ts
+++ src/client/pages/Offer/useGetPromotions.ts
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { hasCar } from 'api/quoteBundleSelectors'
+import { hasCar, isCarSwitcher } from 'api/quoteBundleSelectors'
 import { BundledQuote } from 'data/graphql'
 import { useTextKeys } from 'utils/textKeys'
 import { useFeature } from 'utils/hooks/useFeature'
@@ -21,33 +21,36 @@ export const useGetPromotions = (quotes: BundledQuote[]): Promotion[] => {
   const textKeys = useTextKeys()
   const [isCarCancellationEnbled] = useFeature([Feature.CAR_CANCELLATION])
   const hasCarInBundles = hasCar(quotes)
+  const isSwitcher = isCarCancellationEnbled && isCarSwitcher(quotes)
+
   const promotions = useMemo(() => {
-    return hasCarInBundles
-      ? [
-          {
-            id: PromotionTypes.APP,
-            headingText: textKeys.OFFER_APP_PROMOTION_HEADING_CAR(),
-            bodyText: textKeys.OFFER_APP_PROMOTION_PARAGRAPH_CAR(),
-            imageUrl: '/new-member-assets/offer/app-promotion-car.jpg',
-          },
-          {
-            id: PromotionTypes.SWITCH,
-            headingText: textKeys.OFFER_SWITCH_PROMOTION_HEADING_CAR(),
-            bodyText: isCarCancellationEnbled
-              ? textKeys.OFFER_SWITCH_PROMOTION_PARAGRAPH_CAR_V2()
-              : textKeys.OFFER_SWITCH_PROMOTION_PARAGRAPH_CAR(),
-            imageUrl: '/new-member-assets/offer/switch-promotion.jpg',
-          },
-        ]
-      : [
-          {
-            id: PromotionTypes.APP,
-            headingText: textKeys.OFFER_APP_PROMOTION_HEADING(),
-            bodyText: textKeys.OFFER_APP_PROMOTION_PARAGRAPH(),
-            imageUrl: '/new-member-assets/offer/app-promotion.jpg',
-          },
-        ]
-  }, [hasCarInBundles, textKeys, isCarCancellationEnbled])
+    const appPromo = {
+      id: PromotionTypes.APP,
+      headingText: textKeys.OFFER_APP_PROMOTION_HEADING(),
+      bodyText: textKeys.OFFER_APP_PROMOTION_PARAGRAPH(),
+      imageUrl: '/new-member-assets/offer/app-promotion.jpg',
+    }
+
+    const carAppPromo = {
+      id: PromotionTypes.APP,
+      headingText: textKeys.OFFER_APP_PROMOTION_HEADING_CAR(),
+      bodyText: textKeys.OFFER_APP_PROMOTION_PARAGRAPH_CAR(),
+      imageUrl: '/new-member-assets/offer/app-promotion-car.jpg',
+    }
+
+    const carSwitchingPromo = {
+      id: PromotionTypes.SWITCH,
+      headingText: textKeys.OFFER_SWITCH_PROMOTION_HEADING_CAR(),
+      bodyText: textKeys.OFFER_SWITCH_PROMOTION_PARAGRAPH_CAR_V2(),
+      imageUrl: '/new-member-assets/offer/switch-promotion.jpg',
+    }
+
+    if (!hasCarInBundles) return [appPromo]
+
+    if (!isSwitcher) return [carAppPromo]
+
+    return [carAppPromo, carSwitchingPromo]
+  }, [hasCarInBundles, textKeys, isSwitcher])
 
   return promotions
 }
