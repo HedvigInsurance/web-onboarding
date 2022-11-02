ef4e965eefcaf56b94c633046a166d0ebe47260f
Guilherme Popolin <guilherme@hedvig.com>
2022-11-02 15:32:34 +0100

    feat: enable SE car price matching flow A/B testing

--- src/routes.tsx
+++ src/routes.tsx
@@ -47,6 +47,7 @@ enum EmbarkStory {
   SwedenQuoteCartSwitcher = 'Web Onboarding SE - Quote Cart Switcher',
   SwedenQuoteCartSwitcherV2 = 'Web Onboarding SE - Quote Cart Switcher-v2',
   SwedenCarV2 = 'SE-onboarding-car-v2',
+  SwedenCarV3 = 'SE-onboarding-car-v3',
 }
 
 export type CanonicalLinksPerLocale = Record<LocalePath, string>
@@ -398,6 +399,12 @@ export const routes: Route[] = [
                   return {
                     baseUrl,
                     name: EmbarkStory.SwedenCarV2,
+                    quoteCart: true,
+                  }
+                case 'car-v3':
+                  return {
+                    baseUrl,
+                    name: EmbarkStory.SwedenCarV3,
                     quoteCart: true,
                   }
               }
