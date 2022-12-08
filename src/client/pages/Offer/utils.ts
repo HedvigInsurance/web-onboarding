9eb5e77025e98e2136b80122dace4f90a2173a05
Guilherme Popolin <guilherme@hedvig.com>
2022-12-08 16:29:16 +0100

    fix: ts issues due the inlcusion of swedish trial type of contracts

--- src/client/pages/Offer/utils.ts
+++ src/client/pages/Offer/utils.ts
@@ -305,6 +305,9 @@ export const typeOfResidenceTextKeys: Record<
     'CHECKOUT_DETAILS_RESIDENCE_TYPE_RENT',
   [TypeOfContract.SeApartmentStudentBrf]:
     'CHECKOUT_DETAILS_RESIDENCE_TYPE_OWN_APARTMENT',
+  [TypeOfContract.SeApartmentBrfTrial]:
+    'CHECKOUT_DETAILS_RESIDENCE_TYPE_OWN_APARTMENT',
+  [TypeOfContract.SeApartmentRentTrial]: 'CHECKOUT_DETAILS_RESIDENCE_TYPE_RENT',
   [TypeOfContract.SeHouse]: 'CHECKOUT_DETAILS_RESIDENCE_TYPE_OWN_HOUSE',
   [TypeOfContract.NoHouse]: 'CHECKOUT_DETAILS_RESIDENCE_TYPE_OWN_HOUSE',
   [TypeOfContract.DkHouse]: 'CHECKOUT_DETAILS_RESIDENCE_TYPE_OWN_HOUSE',
