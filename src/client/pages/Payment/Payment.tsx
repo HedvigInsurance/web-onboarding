78393da480619c9cf320c900e68aaf8ef52b0fd7
Robin Andeer <robin.andeer@gmail.com>
2023-01-16 13:59:06 +0100

    Update design of Payment page (hedvig-com) (#1232)
    ## What?
    
    Update design of Payment page (hedvig-com checkout) to better match latest designs.
    
    ![Screenshot 2023-01-12 at 16.31.36.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/b7b81700-b9ef-49a6-a435-fcb8a8436ff9/Screenshot%202023-01-12%20at%2016.31.36.png)
    
    ![Screenshot 2023-01-12 at 16.31.46.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OgXegTwxM9IeuXHZyw5h/71025d10-07b5-426a-bb9b-c6cb2a78519e/Screenshot%202023-01-12%20at%2016.31.46.png)
    
    ## Why?
    
    <!-- Why are these changes made? -->
    
    
    
    **Ticket(s): []**
    <!-- If there is a Jira issue, add the key (e.g. GRW-123) between the brackets, and a link to that issue will automatically be created. -->
    
    
    <!-- If it makes sense, add screenshots and/or screen recordings below, with headlines and/or descriptions if needed. -->
    <!--
    ## Screenshots / recordings 
    -->
    
    <!-- Finally, you can create a review app on Heroku to make it easier to review and/or get input from the design team before merging. -->
    <!--
    ### [Review app]()
    -->

--- src/client/pages/Payment/Payment.tsx
+++ src/client/pages/Payment/Payment.tsx
@@ -5,9 +5,16 @@ import { SessionTokenGuard } from 'containers/SessionTokenGuard'
 import { HedvigLogo } from 'components/icons/HedvigLogo'
 import { useCurrentLocale } from 'l10n/useCurrentLocale'
 import { useTextKeys } from 'utils/textKeys'
+import { MEDIA_QUERIES } from 'utils/mediaQueries'
+import { LocaleData } from 'l10n/locales'
 import { useCreateTrustlyURL } from './useCreateTrustlyURL'
 import { TrustlyIframe } from './TrustlyIframe'
 import { useStoredRedirectURL } from './useStoredRedirectURL'
+
+const CUSTOMER_SERVIVE_URL: Partial<Record<LocaleData['path'], string>> = {
+  se: '/se/hjalp/kundservice',
+  'se-en': '/se-en/help/customer-service',
+}
 
 export const PaymentPage = () => {
   const textKeys = useTextKeys()
@@ -27,9 +34,15 @@ export const PaymentPage = () => {
     <SessionTokenGuard>
       <Wrapper>
         <PageHeader>
-          <LogoLink href={'/' + pathLocale}>
-            <HedvigLogo width={82} />
-          </LogoLink>
+          <HedvigLogo width={82} />
+
+          <HeaderLink
+            href={CUSTOMER_SERVIVE_URL[pathLocale]}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {textKeys.AFTER_SIGN_PAYMENT_HELP_LINK()}
+          </HeaderLink>
         </PageHeader>
         <Header>
           <Heading>{textKeys.ONBOARDING_CONNECT_DD_CTA()}</Heading>
@@ -55,19 +68,29 @@ const Wrapper = styled.div({
   display: 'flex',
   flexDirection: 'column',
   gap: '2rem',
-  padding: '1rem',
+  paddingBottom: '2rem',
 })
 
 const PageHeader = styled.div({
+  height: '3.5rem',
+  paddingInline: '1rem',
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+
+  [MEDIA_QUERIES.mediumScreen]: {
+    paddingInline: '2rem',
+  },
 })
 
-const LogoLink = styled.a({
-  display: 'block',
-  color: 'inherit',
-  // remove extra space under child SVG: https://stackoverflow.com/a/51161925
-  fontSize: 0,
+const HeaderLink = styled.a({
+  color: colorsV3.gray900,
+  textDecoration: 'none',
+
+  ':hover': {
+    color: colorsV3.gray900,
+    textDecoration: 'underline',
+  },
 })
 
 const Header = styled.div({
@@ -75,15 +98,24 @@ const Header = styled.div({
   flexDirection: 'column',
   gap: '1rem',
   maxWidth: '37rem',
-  margin: '0 auto',
+  paddingInline: '1rem',
+  marginInline: 'auto',
+
+  [MEDIA_QUERIES.mediumScreen]: {
+    gap: '1.5rem',
+  },
 })
 
 const Heading = styled.h1({
   margin: 0,
-  fontSize: '1.5rem',
+  fontSize: '2rem',
   textAlign: 'center',
   fontFamily: fonts.HEDVIG_LETTERS_STANDARD,
   fontWeight: 400,
+
+  [MEDIA_QUERIES.mediumScreen]: {
+    fontSize: '3rem',
+  },
 })
 
 const Paragraph = styled.p({
