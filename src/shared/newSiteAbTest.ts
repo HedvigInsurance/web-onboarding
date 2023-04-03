e7ca7cce3f0e0c8ee7a727282d3711af94f14b4f
Robin Andeer <robin.andeer@gmail.com>
2023-04-03 10:04:07 +0200

    Update Google Optimize experiment ID

--- src/shared/newSiteAbTest.ts
+++ src/shared/newSiteAbTest.ts
@@ -10,7 +10,7 @@ export type AbTestConfig = {
 }
 
 export const newSiteAbTest: AbTestConfig = {
-  optimizeExperimentId: 'wmRyD1ofQYSQ5LL5BEH9nw',
+  optimizeExperimentId: 'mr_3juNyS1yRodkUannTjA',
   cookies: {
     variant: {
       name: 'HEDVIG_EXP_NEW_SITE',
